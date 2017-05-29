7c109d7d7ecf041acc8ba20c715c4ac55642b705
Julie Newcomb <newcombj@cs.washington.edu>
2017-05-29 16:08:14 -0700

    change type to build graph out of functions

--- racketimpl/rosettefjapi.rkt
+++ racketimpl/rosettefjapi.rkt
@@ -1,20 +1,29 @@
 #lang rosette/safe
 (provide (all-defined-out))
 
-(define (oneE evt-stream)
-  (list (first evt-stream))) ;; propagate only the first event
+(define (oneE evt-stream-f)
+  (λ ()
+    (let ([evt-stream (evt-stream-f)])
+      (list (first evt-stream))))) ;; propagate only the first event
 
-(define (zeroE evt-stream)
-  (void)) ;; a stream that never fires
+(define (zeroE evt-stream-f)
+  (λ ()
+  (void))) ;; a stream that never fires
 
-(define (mapE proc evt-stream)
-  (map (λ (e) (list (first e) (proc (second e)))) evt-stream))
+(define (mapE proc evt-stream-f)
+  (λ ()
+    (let ([evt-stream (evt-stream-f)])
+  (map (λ (e) (list (first e) (proc (second e)))) evt-stream))))
 
-(define (mergeE s-events1 s-events2)
-  (sort (append s-events1 s-events2) (λ (x y) (< (first x) (first y)))))
+(define (mergeE evt-stream1-f evt-stream2-f) ;; note: mergeE can actually take any num of args
+  (λ ()
+    (let ([evt-stream1 (evt-stream1-f)]
+          [evt-stream2 (evt-stream2-f)])
+      (sort (append evt-stream1 evt-stream2) (λ (x y) (< (first x) (first y)))))))
 
 (define (switchE stream-of-streams)
-  (sort (flatten stream-of-streams) (λ (x y) (< (first x) (first y)))))
+  (λ ()
+  (sort (flatten stream-of-streams) (λ (x y) (< (first x) (first y))))))
 
 ;; condE
 
@@ -22,18 +31,22 @@
 
 ;; ifE
 
-(define (constantE s-events const)
-  (map (λ (s) (list (first s) const)) s-events))
+(define (constantE evt-stream-f const)
+  (λ ()
+    (let ([evt-stream (evt-stream-f)])
+      (map (λ (s) (list (first s) const)) evt-stream))))
 
-(define (collectE s-behavior init proc)
-  (letrec ([collect (λ (x-lst prev)
-                      (if (equal? (length x-lst) 0)
-                          '()
-                          (let ([new-ts (first (first x-lst))]
-                                [new-val (proc (second (first x-lst)) prev)])
-                            (append (list (list new-ts new-val))
-                                    (collect (cdr x-lst) new-val)))))])
-    (collect s-behavior init)))
+(define (collectE evt-stream-f init proc)
+  (λ ()
+    (let ([evt-stream (evt-stream-f)])
+      (letrec ([collect (λ (x-lst prev)
+                          (if (equal? (length x-lst) 0)
+                              '()
+                              (let ([new-ts (first (first x-lst))]
+                                    [new-val (proc (second (first x-lst)) prev)])
+                                (append (list (list new-ts new-val))
+                                        (collect (cdr x-lst) new-val)))))])
+        (collect evt-stream init)))))
 
 ;; andE
 
@@ -51,22 +64,28 @@
 
 ;; skipFirstE
 
-(define (delayE evt-stream interval)
-  (map (λ(s) (list (+ (first s) interval) (second s))) evt-stream))
+(define (delayE evt-stream-f interval)
+  (λ ()
+    (let ([evt-stream (evt-stream-f)])
+      (map (λ(s) (list (+ (first s) interval) (second s))) evt-stream))))
 
 ;; blindE
 
-(define (calmE evt-stream interval)
-  (letrec ([calm (λ (evt-lst)
-                  (if (equal? (length evt-lst) 1)
-                      evt-lst ;; the last event is always propagated
-                      (let ([current (first evt-lst)]
-                            [next (second evt-lst)])
-                        (if (< (- (first next) (first current)) interval)
-                            (calm (cdr evt-lst)) ;; the first one is too close to the second, don't propagate
-                            (append (list current) (calm (cdr evt-lst)))))))]) ;; propagate the first one
-    (calm evt-stream)))
+(define (calmE evt-stream-f interval)
+  (λ ()
+    (let ([evt-stream (evt-stream-f)])
+      (letrec ([calm (λ (evt-lst)
+                       (if (equal? (length evt-lst) 1)
+                           evt-lst ;; the last event is always propagated
+                           (let ([current (first evt-lst)]
+                                 [next (second evt-lst)])
+                             (if (< (- (first next) (first current)) interval)
+                                 (calm (cdr evt-lst)) ;; the first one is too close to the second, don't propagate
+                                 (append (list current) (calm (cdr evt-lst)))))))]) ;; propagate the first one
+    (calm evt-stream)))))
 
-(define (startsWith s-events init) ;; adds special timestep 0
-  (append (list (list 0 init)) s-events))
+(define (startsWith evt-stream-f init) ;; adds special timestep 0
+  (λ ()
+    (let ([evt-stream (evt-stream-f)])
+      (append (list (list 0 init)) evt-stream))))
 
--- racketimpl/symbolictsfj.rkt
+++ racketimpl/symbolictsfj.rkt
@@ -7,24 +7,25 @@
 ;; behaviors consist of timestamp/value pairs when some value *changes* (if no entry, behavior holds its previous value)
 
 (define (clicksE concrete-list)
-  (map (λ (c)
-                  (define-symbolic* timestamp integer?)
-                  (list timestamp 'click)) concrete-list))
+  (λ ()
+    (map (λ (c)
+         (define-symbolic* timestamp integer?)
+         (list timestamp 'click)) concrete-list)))
 
 
 
 (define (inc-dec-button-graph inc dec)
   (startsWith (collectE (mergeE (constantE inc 1) (constantE dec -1)) 0 +) 0))
 
-(define concrete-inc-clicks (list (list 1 'click) (list 4 'click)))
-(define concrete-dec-clicks (list (list 2 'click) (list 3 'click) (list 5 'click)))
+(define concrete-inc-clicks (λ () (list (list 1 'click) (list 4 'click))))
+(define concrete-dec-clicks (λ () (list (list 2 'click) (list 3 'click) (list 5 'click))))
 
 (displayln (inc-dec-button-graph concrete-inc-clicks concrete-dec-clicks))
 
 (define s-inc (clicksE (list 1)))
 (define s-dec (clicksE (list 1)))
 
-;(displayln (inc-dec-button-graph s-inc s-dec))
+(displayln ((inc-dec-button-graph s-inc s-dec)))
 
 ;;;;; mouse tail ;;;;
 
@@ -32,18 +33,18 @@
   (map (λ (x) (define-symbolic* timestamp integer?) timestamp) concrete-list))
 
 (define (integer-events-for-timestamps timestamps)
-  (map (λ (ts) (define-symbolic* value integer?) (list ts value)) timestamps))
+  (λ () (map (λ (ts) (define-symbolic* value integer?) (list ts value)) timestamps)))
 
 (define (integer-event-list concrete-list)
-  (map (λ (c)
+  (λ () (map (λ (c)
          (define-symbolic* timestamp integer?)
          (define-symbolic* value integer?)
-         (list timestamp value)) concrete-list))
+         (list timestamp value)) concrete-list)))
 
 (define (mouse-tail-graph mouse-x-event-stream mouse-y-event-stream)
   (let ([tail-x (mapE (λ (e) (+ e 50)) (delayE mouse-x-event-stream 3))]
         [tail-y (delayE mouse-y-event-stream 3)])
-    (list tail-x tail-y)))
+    (list (tail-x) (tail-y))))
 
 (define mouse-timestamps (timestamp-list (list 1 2 3)))
 (define mouse-x (integer-events-for-timestamps mouse-timestamps))
@@ -51,9 +52,9 @@
 
 (displayln (mouse-tail-graph mouse-x mouse-y))
 
-(define (unique-timestamps timestamps)
-  (for-each ([ts timestamps])
-(define mouse-tail-sol (solve (assert
+;;(define (unique-timestamps timestamps)
+;;  (for-each ([ts timestamps])
+;;(define mouse-tail-sol (solve (assert
 
 ;;;;;; drag and drop ;;;;;
 
@@ -64,7 +65,7 @@
 (define (light-graph md-events)
   (mergeE (constantE md-events 'on) (constantE (calmE (delayE md-events 5) 5) 'off)))
 
-(define concrete-motion (list (list 1 'd) (list 3 'd) (list 10 'd) (list 20 'd)))
+(define concrete-motion (λ () (list (list 1 'd) (list 3 'd) (list 10 'd) (list 20 'd))))
 
 (define s-motion (clicksE (list 1 2)))
 
