781a2a96502574fdf25133c1b47183e104a0e521
Julie Newcomb <newcombj@cs.washington.edu>
2017-06-05 13:34:27 -0700

    concrete tests

--- racketimpl/benchmarks/incdecbutton.rkt
+++ racketimpl/benchmarks/incdecbutton.rkt
@@ -3,23 +3,23 @@
 (require "../rosettefjapi.rkt")
 
 (define (clicksE concrete-list)
- (λ ()
+ ;(λ ()
     (map (λ (c)
          (define-symbolic* timestamp integer?)
-         (list timestamp 'click)) concrete-list)))
+         (list timestamp 'click)) concrete-list));)
 
 (define (inc-dec-button-graph inc dec)
   (startsWith
-  (collectE
-               (mergeE (constantE inc 1) (constantE dec -1)) 0 +)  (λ () (list (list 0 70)))))
+   (collectE
+   (mergeE (constantE inc 1) (constantE dec -1)) 0 +) 0))
 
 (define concrete-inc-clicks (λ () (list (list 1 'click) (list 4 'click))))
 (define concrete-dec-clicks (λ () (list (list 2 'click) (list 3 'click) (list 5 'click))))
 
 (displayln ((inc-dec-button-graph concrete-inc-clicks concrete-dec-clicks)))
 
-(define s-inc (clicksE (list )))
-(define s-dec (clicksE (list )))
+(define s-inc (clicksE (list 1)))
+(define s-dec (clicksE (list 1)))
 
 (define button-sol (solve
                     (begin
--- racketimpl/test-fjapi.rkt
+++ racketimpl/test-fjapi.rkt
@@ -7,7 +7,7 @@
       (printf "~a test successful!\n" test-name)
       (printf "~a test failed! expected ~a, got ~a\n" test-name test-code expected)))
 
-(define standard-evt-stream (λ () (list (list 1 'one) (list 2 'two) (list 3 'three))))
+(define standard-evt-stream (λ () (list (list 1 11) (list 2 '12) (list 3 13))))
 (define empty-evt-stream (λ () '()))
 
 ;; oneE tests
@@ -21,11 +21,14 @@
 (test "zeroE on empty stream" ((zeroE empty-evt-stream)) '())
 
 ;; mapE tests
+(test "mapE" ((mapE (λ (e) (list (first e) (add1 (second e)))) standard-evt-stream)) (list (list 1 12) (list 2 13) (list 3 14)))
+(test "mapE on empty stream" ((mapE (λ (e) (list (first e) (add1 (second e)))) empty-evt-stream)) '())
 
-
-
+;; collectE tests
+(test "collectE" ((collectE standard-evt-stream 0 (λ (old new) (+ old new)))) (list (list 1 11) (list 2 23) (list 3 36)))
+(test "collectE on empty stream)" ((collectE empty-evt-stream 0 (λ (old new) (+ old new)))) '())
 
 ;; startsWith tests
-(test "startsWith" ((startsWith standard-evt-stream 'zero))
-      (list (list 0 'zero) (list 1 'one) (list 2 'two) (list 3 'three)))
+(test "startsWith" ((startsWith standard-evt-stream 10))
+      (list (list 0 10) (list 1 11) (list 2 12) (list 3 13)))
 (test "startsWith on empty stream" ((startsWith empty-evt-stream 'zero)) (list (list 0 'zero)))
