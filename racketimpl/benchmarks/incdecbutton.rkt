a92871537486d89bc8b8c1225c2514875b56edf6
Julie Newcomb <newcombj@cs.washington.edu>
2017-06-16 10:57:29 -0700

    clean up button benchmark

--- racketimpl/benchmarks/incdecbutton.rkt
+++ racketimpl/benchmarks/incdecbutton.rkt
@@ -32,37 +32,45 @@
 (define (concrete-eval inc dec)
   ((inc-dec-button-graph (λ () inc) (λ () dec))))
 
+(define (button-assumptions inc-stream dec-stream)
+    (and (valid-input-timestamps? inc-stream)
+         (valid-input-timestamps? dec-stream)
+         (apply distinct? (map get-timestamp (append inc-stream dec-stream)))
+         (andmap (λ (e) (or (equal? 'no-evt (get-value e)) (equal? 'click (get-value e)))) (append inc-stream dec-stream))))
 
-
+(define (button-guarantees output-stream)
+    (and (valid-output-timestamps? output-stream)
+         (equal? (get-value (first output-stream)) 0)
+         (equal? (get-timestamp (first output-stream)) 0)
+         (andmap integer? (map get-value output-stream))
+         (equal? (get-value (last output-stream)) (- (length (filter (λ (e) (equal? 'click (get-value e))) s-inc))
+                                                                                      (length (filter (λ (e) (equal? 'click (get-value e))) s-dec))))
+         ))
 
 (define verified (verify
                   ;; assume that all inputs are valid
-                  #:assume (assert (and
-                                    ;; all timestamps are distinct
-                                    (apply distinct? (map get-timestamp (append s-dec s-inc)))
-                                    ;;(apply distinct? (map get-timestamp s-inc))
-                                    ;;(apply distinct? (map get-timestamp s-dec))
-                                    ;; all timestamps are integers
-                                    (andmap integer? (map get-timestamp (append s-dec s-inc)))
-                                    ;; all timestamps are positive numbers
-                                    (andmap positive? (map get-timestamp (append s-dec s-inc)))
-                                    ;; lists of timestamps are sorted
-                                    (equal? (map get-timestamp s-inc) (sort (map get-timestamp s-inc) <))
-                                    (equal? (map get-timestamp s-dec) (sort (map get-timestamp s-dec) <))
-                                    ;; input values are either 'click or 'no-evt
-                                    (andmap (λ (e) (or (equal? 'no-evt (get-value e)) (equal? 'click (get-value e)))) (append s-inc s-dec))
-                                    ))
-                  #:guarantee (let ([output-evt-stream ((inc-dec-button-graph (λ () s-inc) (λ () s-dec)))])
-                                (assert (and
-                                         ;; output timestamps are sorted
-                                         (equal? (map get-timestamp output-evt-stream) (sort (map get-timestamp output-evt-stream) <))
-                                         ;; first value at special timestamp 0 is 0
-                                         (eq? (get-value (first output-evt-stream)) 0)
-                                         (eq? (get-timestamp (first output-evt-stream)) 0)
-                                         ;; all values are integers (i.e. all values are defined
-                                         (andmap integer? (map get-value output-evt-stream))
-                                         ;; the final value should be the number of inc button clicks - number of dec button clicks
-                                         (eq? (get-value (last output-evt-stream)) (- (length (filter (λ (e) (equal? 'click (get-value e))) s-inc))
-                                                                                      (length (filter (λ (e) (equal? 'click (get-value e))) s-dec))))
-                                         )))
-                                ))
+                  #:assume (assert (button-assumptions s-inc s-dec))
+                  #:guarantee (assert (button-guarantees ((inc-dec-button-graph (λ () s-inc) (λ () s-dec)))))
+                  ))
+
+#;(define verified2 (verify
+                   #:assume (begin ;;(assert
+                           ;; (assert (apply distinct? (map get-timestamp (append s-dec s-inc))))
+                           ;;(assert (apply distinct? (map get-timestamp s-inc)))
+                           ;;(assert (apply distinct? (map get-timestamp s-dec)))
+                           ;;   (assert (andmap (λ (e) (not (member (get-timestamp e) (map get-timestamp s-dec)))) s-inc))
+                            (assert (andmap positive? (map get-timestamp s-inc)))
+                            (assert (andmap positive? (map get-timestamp s-dec)))
+                          ;; (assert (equal? (map get-timestamp s-inc) (sort (map get-timestamp s-inc) <)))
+                          ;; (assert (equal? (map get-timestamp s-dec) (sort (map get-timestamp s-dec) <)))
+                                 ;;  (assert (apply distinct? (map get-timestamp s-dec))))
+                            )
+                   #:guarantee ;;(assert (or (equal? 1 (get-value (first ((small-graph (λ () s-inc) (λ () s-dec))))))
+                                 ;;      (equal? -1 (get-value (first ((small-graph (λ () s-inc) (λ () s-dec))))))))))
+                   (assert (equal? (get-value (first ((inc-dec-button-graph (λ () s-inc) (λ () s-dec))))) 0))))
+
+
+(define-symbolic x integer?)
+(define v (verify
+           #:assume (assert (equal? x 2))
+           #:guarantee (assert (equal? 0 (/ x 0)))))
--- racketimpl/rosettefjapi.rkt
+++ racketimpl/rosettefjapi.rkt
@@ -15,6 +15,22 @@
 (define (get-value item)
   (second item))
 
+(define (valid-input-timestamps? input-stream)
+  (and (apply distinct? (map get-timestamp input-stream))
+       (andmap integer? (map get-timestamp input-stream))
+       (andmap positive? (map get-timestamp input-stream))
+       (timestamps-sorted? input-stream)))
+
+(define (valid-output-timestamps? output-stream)
+    (and (apply distinct? (map get-timestamp output-stream))
+       (andmap integer? (map get-timestamp output-stream))
+       ;; outputs can have 0 timestamp
+       (andmap (λ (n) (not (negative? n))) (map get-timestamp output-stream))
+       (timestamps-sorted? output-stream)))
+
+(define (timestamps-sorted? stream)
+  (equal? (map get-timestamp stream) (sort (map get-timestamp stream) <)))
+
 ;;;;; flapjax API ;;;;;
 
 (define (oneE evt-stream-f)
@@ -37,13 +53,13 @@
   (λ ()
     (let ([evt-stream1 (evt-stream1-f)]
           [evt-stream2 (evt-stream2-f)])
-      (sort (append evt-stream1 evt-stream2) (λ (x y) (< (first x) (first y)))))))
+      (sort (append evt-stream1 evt-stream2) (λ (x y) (< (get-timestamp x) (get-timestamp y)))))))
 
 (define (switchE stream-of-streams-f)
   (λ ()
     (let ([stream-of-streams (map (λ (f) (apply (second f) '()))
                                   stream-of-streams-f)])
-      (sort (flatten stream-of-streams) (λ (x y) (< (first x) (first y)))))))
+      (sort (flatten stream-of-streams) (λ (x y) (< (get-timestamp x) (get-timestamp y)))))))
 
 ;; condE
 
@@ -54,7 +70,7 @@
 (define (constantE evt-stream-f const)
   (λ ()
     (let ([evt-stream (evt-stream-f)])
-      (map (λ (s) (list (first s) (if (equal? 'no-evt (second s)) 'no-evt const))) evt-stream))))
+      (map (λ (s) (list (get-timestamp s) (if (equal? 'no-evt (get-value s)) 'no-evt const))) evt-stream))))
 
 (define (collectE evt-stream-f init proc)
   (λ ()
@@ -62,9 +78,9 @@
       (letrec ([collect (λ (x-lst prev)
                           (if (equal? (length x-lst) 0)
                               '()
-                              (let* ([new-ts (first (first x-lst))]
-                                    [input-val (second (first x-lst))]
-                                    [new-val (if (equal? input-val 'no-evt) prev (proc (second (first x-lst)) prev))])
+                              (let* ([new-ts (get-timestamp (first x-lst))]
+                                    [input-val (get-value (first x-lst))]
+                                    [new-val (if (equal? input-val 'no-evt) prev (proc (get-value (first x-lst)) prev))])
                                 (append (list (list new-ts new-val))
                                         (collect (cdr x-lst) new-val)))))])
         (collect evt-stream init)))))
@@ -88,7 +104,7 @@
 (define (delayE evt-stream-f interval)
   (λ ()
     (let ([evt-stream (evt-stream-f)])
-      (map (λ(s) (list (+ (first s) interval) (second s))) evt-stream))))
+      (map (λ(s) (list (+ (get-timestamp s) interval) (get-value s))) evt-stream))))
 
 ;; blindE
 
