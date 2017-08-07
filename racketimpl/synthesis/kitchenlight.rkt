2f3c9436278a64ae74fd923900cb62e788273fc4
Julie Newcomb <newcombj@cs.washington.edu>
2017-08-07 16:33:38 -0700

    kitchen light synthesis

--- racketimpl/synthesis/kitchenlight.rkt
+++ racketimpl/synthesis/kitchenlight.rkt
@@ -29,7 +29,7 @@
 (define (kitchen-light-color-graph kitchenLightB modeB)
   (liftB (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none)) kitchenLightB modeB))
 (define (synth-kitchen-light-color-graph kitchenLightB modeB)
-  (flapjax-grmrB2 kitchenLightB modeB 3))
+  (flapjax-grmrB3 kitchenLightB modeB '() 3))
 
 (define (full-kitchen-light-color-graph clockB userLocationB motionSensorB)
   (liftB (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none))
@@ -41,7 +41,7 @@
                                      'away)))
          clockB userLocationB)))
 (define (synth-full-kitchen-light-color-graph clockB userLocationB motionSensorB)
-  (flapjax-grmrB3 clockB userLocationB motionSensorB 3))
+  (flapjax-grmrB3 clockB userLocationB motionSensorB 4))
 
 ;; location behavior symbolic variables
 (define-symbolic* init-bool boolean?)
@@ -94,6 +94,40 @@
                          (list timestamp8 (if location2 'home 'not-at-home))
                          (list timestamp9 (if location3 'home 'not-at-home))))))
 
+;; mode behavior symbolic variables
+(define-symbolic* init-mode-night boolean?)
+(define-symbolic* init-mode-home boolean?)
+(define-symbolic* timestamp10 integer?)
+(define-symbolic* mode-night1 boolean?)
+(define-symbolic* mode-home1 boolean?)
+(define-symbolic* timestamp11 integer?)
+(define-symbolic* mode-night2 boolean?)
+(define-symbolic* mode-home2 boolean?)
+(define-symbolic* timestamp12 integer?)
+(define-symbolic* mode-night3 boolean?)
+(define-symbolic* mode-home3 boolean?)
+
+(assert (valid-behavior?
+         (behavior (if init-mode-night 'night (if init-mode-home 'home 'away))
+                   (list (list timestamp10 (if mode-night1 'night (if mode-home1 'home 'away)))
+                         (list timestamp11 (if mode-night2 'night (if mode-home1 'home 'away)))
+                         (list timestamp12 (if mode-night3 'night (if mode-home1 'home 'away)))))))
+
+;; kitchen light symbolic variables
+(define-symbolic* init-light boolean?)
+(define-symbolic* timestamp13 integer?)
+(define-symbolic* light1 boolean?)
+(define-symbolic* timestamp14 integer?)
+(define-symbolic* light2 boolean?)
+(define-symbolic* timestamp15 integer?)
+(define-symbolic* light3 boolean?)
+
+(assert (valid-behavior?
+         (behavior (if init-light 'on 'off)
+                   (list (list timestamp13 (if light1 'on 'off))
+                         (list timestamp14 (if light2 'on 'off))
+                         (list timestamp15 (if light3 'on 'off))))))
+
 (displayln "Synthesize mode graph")
 (define mode-begin-time (current-seconds))
 (define mode-binding
@@ -144,31 +178,48 @@
 (displayln "Synthesize partial kitchen light color graph")
 (define light-color-partial-begin-time (current-seconds))
 (define light-color-partial-binding
-  (synthesize #:forall (list timestamp4 hour1 ten1 minute1
-                             timestamp5 hour2 ten2 minute2
-                             timestamp6 hour3 ten3 minute3
-                             timestamp7 location1
-                             timestamp8 location2
-                             timestamp9 location3
-                             init-hour init-ten init-minute
-                             init-location
-                             timestamp1 bool1
-                             timestamp2 bool2
-                             timestamp3 bool3
-                             init-bool
+  (synthesize #:forall (list ;timestamp4 hour1 ten1 minute1
+                             ;timestamp5 hour2 ten2 minute2
+                             ;timestamp6 hour3 ten3 minute3
+                             ;timestamp7 location1
+                             ;timestamp8 location2
+                             ;timestamp9 location3
+                             ;init-hour init-ten init-minute
+                             ;init-location
+                             ;timestamp1 bool1
+                             ;timestamp2 bool2
+                             ;timestamp3 bool3
+                             ;init-bool
+                        init-mode-night init-mode-home
+                        timestamp10 mode-night1 mode-home1
+                        timestamp11 mode-night2 mode-home2
+                        timestamp12 mode-night3 mode-home3
+                        init-light
+                        timestamp13 light1
+                        timestamp14 light2
+                        timestamp15 light3
                              )
               #:guarantee (same2 kitchen-light-color-graph synth-kitchen-light-color-graph
-                                 (mode-graph (behavior (vector init-hour init-ten init-minute)
-                                                       (list (list timestamp4 (vector hour1 ten1 minute1))
-                                                             (list timestamp5 (vector hour2 ten2 minute2))
-                                                             (list timestamp6 (vector hour3 ten3 minute3))))
-                                             (behavior (if init-location 'home 'not-at-home)
+                                 #;(mode-graph (behavior (vector init-hour init-ten init-minute)
+                                           (list (list timestamp4 (vector hour1 ten1 minute1))
+                                                 (list timestamp5 (vector hour2 ten2 minute2))
+                                                 (list timestamp6 (vector hour3 ten3 minute3))))
+                                 (behavior (if init-location 'home 'not-at-home)
                                            (list (list timestamp7 (if location1 'home 'not-at-home))
                                                  (list timestamp8 (if location2 'home 'not-at-home))
                                                  (list timestamp9 (if location3 'home 'not-at-home)))))
-                                 (kitchen-light-status-graph (behavior init-bool (list (list timestamp1 bool1)
-                                                  (list timestamp2 bool2)
-                                                  (list timestamp3 bool3)))))))
+                                 (behavior (if init-mode-night 'night (if init-mode-home 'home 'away))
+                                           (list (list timestamp10 (if mode-night1 'night (if mode-home1 'home 'away)))
+                                                 (list timestamp11 (if mode-night2 'night (if mode-home1 'home 'away)))
+                                                 (list timestamp12 (if mode-night3 'night (if mode-home1 'home 'away)))))
+                                 #;(kitchen-light-status-graph
+                                  (behavior init-bool (list (list timestamp1 bool1)
+                                                            (list timestamp2 bool2)
+                                                            (list timestamp3 bool3))))
+                                 (behavior (if init-light 'on 'off)
+                                           (list (list timestamp13 (if light1 'on 'off))
+                                                 (list timestamp14 (if light2 'on 'off))
+                                                 (list timestamp15 (if light3 'on 'off)))))))
 (define light-color-partial-end-time (current-seconds))
 (if (unsat? light-color-partial-binding)
     (displayln "No binding was found.")
