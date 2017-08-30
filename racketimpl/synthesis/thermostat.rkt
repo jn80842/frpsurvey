d1b8f4ea52c59eb4a4cc08a5effd84a0dee26833
Julie Newcomb <newcombj@cs.washington.edu>
2017-08-29 17:34:26 -0700

    thermostat

--- racketimpl/synthesis/thermostat.rkt
+++ racketimpl/synthesis/thermostat.rkt
@@ -4,33 +4,66 @@
 (require "../fjmodels.rkt")
 (require "../rosettefjapi.rkt")
 (require "../benchmarks/thermostat.rkt")
-(require "grammar.rkt")
+;(require "grammar.rkt")
+
+;;                    ifB
+;;            /        |         \
+;;          andB    constantB    constantB
+;;        /    \        |            |
+;;    liftB    liftB   'on          'off
+;;   /    \     /   \
+;;  λ   tempB  λ   clockB
+
 (define-synthax (flapjax-grmr input ... depth)
   #:base (choose input ...)
   #:else (choose input ...
                  ;; zero arity
-                 (zeroE)
+                 ;(zeroE)
                  (constantB (choose 'on 'off (??)))
+                 ;; E ::= arity-1-op E
+                 ;((choose oneE
+                 ;  switchE
+                 ;  notE
+                 ;  changes
+                 ;  ) (flapjax-grmr input ... (sub1 depth)))
                  ;; E ::= arity-1-op val E
-                 ((choose startsWith constantE delayE blindE calmE mapE liftB)
-                  (choose (??)
-                          (λ (e) (if e 'on 'off))
-                          (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none))
+                 ((choose
+                  ; startsWith
+                  ; constantE
+                  ; delayE
+                  ; blindE
+                  ; calmE
+                  ; mapE
+                  ; filterE
+                   liftB
+                   )
+                  (choose ;(??)
+                         ; (λ (e) (if e 'on 'off))
+                         ; (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none))
                           (λ (t) (<= t (??)))
                           (λ (c) (or (>= (vector-ref c 0) (??)) (>= (??) (vector-ref c 0))))
-                          (λ (e) (list (get-timestamp e) (+ (get-value e) (??)))))
+                         ; (λ (e) (list (get-timestamp e) (+ (get-value e) (??))))
+                          )
                   (flapjax-grmr input ... (sub1 depth)))
+                 ;; E ::= collectE init λ E
+                 ;(collectE (??) + (flapjax-grmr input ... (sub1 depth)))
                  ;; E ::= arity-2-op E E
-                 ((choose andB mergeE) (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)))
-               ;  (liftB (choose (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none))
-               ;                 (λ (clock location) (if (or (>= (time-vec->integer clock) 2130) (< (time-vec->integer clock) 800))
-               ;                  'night
-               ;                  (if (equal? location 'home)
-               ;                      'home
-               ;                      'away))))
-               ;         (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)))
+                 ((choose andB
+                          ;mergeE
+                          ;snapshotE
+                          ) (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)))
+                 ;; E ::= liftB λ E E
+                 ;(liftB (choose (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none))
+                 ;               (λ (clock location) (if (or (>= (time-vec->integer clock) 2130) (< (time-vec->integer clock) 800))
+                 ;                'night
+                 ;                (if (equal? location 'home)
+                 ;                    'home
+                 ;                    'away))))
+                 ;       (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)))
                  ;; E ::= arity-3-op E E E               
-                 (ifB (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)))
+                 ((choose ;ifE
+                          ifB
+                          ) (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)))          
                  ))
 
 (current-bitwidth 6)
