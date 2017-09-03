eb09946014a8cc0be2b82b0502508c905a449ed7
Julie Newcomb <newcombj@cs.washington.edu>
2017-09-02 22:54:50 -0700

    two versions of the grammar

--- racketimpl/synthesis/grammar.rkt
+++ racketimpl/synthesis/grammar.rkt
@@ -63,13 +63,63 @@
                           ) (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)))          
                  )) 
 
-#;(define-synthax (flapjaxE-grmr input-stream ... depth)
-  #:base (choose input-stream  ... )
-  #:else (choose input-stream  ...
-                (startsWith (flapjaxE-grmr input-stream ... (sub1 depth)) (??))
-                 (collectE (flapjaxE-grmr input-stream ... (sub1 depth)) (??) +)
-                 (mapE (λ (e) (list (get-timestamp e) (+ (get-value e) (??))))
-                       (flapjaxE-grmr input-stream ... (sub1 depth)))
-                 (constantE (flapjaxE-grmr input-stream ... (sub1 depth)) (??))
-                 (delayE (flapjaxE-grmr input-stream ... (sub1 depth)) (??))
-                 (mergeE (flapjaxE-grmr input-stream ... (sub1 depth)) (flapjaxE-grmr input-stream ... (sub1 depth)))))
+
+#;(define-synthax (flapjax-grmr input ... depth)
+  #:base (choose input ...)
+  #:else (let ([recursive-call1 (flapjax-grmr input ... (sub1 depth))]
+               [recursive-call2 (flapjax-grmr input ... (sub1 depth))]
+               [recursive-call3 (flapjax-grmr input ... (sub1 depth))])
+           (choose input ...
+                   ;; zero arity
+                   (zeroE)
+                   (constantB (choose 'on 'off (??)))
+                   ;; E ::= arity-1-op E
+                   ((choose oneE
+                            switchE
+                            notE
+                            changes
+                   ) recursive-call1)
+                 ;; E ::= arity-1-op val E
+                 ((choose
+                   startsWith
+                   constantE
+                   delayE
+                   blindE
+                   calmE
+                   mapE
+                   filterE
+                   liftB
+                   )
+                  (choose (??)
+                          'on 'off
+                          (λ (e) (if e 'on 'off))
+                          (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none))
+                          (λ (t) (<= t (??)))
+                          (λ (c) (or (>= (vector-ref c 0) (??)) (>= (??) (vector-ref c 0))))
+                          (λ (e) (list (get-timestamp e) (+ (get-value e) (??))))
+                          (λ (e) (list (get-timestamp e)
+                           (startBehaviorAtTimestamp (get-timestamp e) (choose input ...))))
+                          (λ (e) (list (get-timestamp e) (zeroE)))
+                          )
+                  recursive-call1)
+                 ;; E ::= collectE init λ E
+                 (collectE (??) + recursive-call1)
+                 ;; E ::= arity-2-op E E
+                 ((choose andB
+                          mergeE
+                          snapshotE
+                          ) recursive-call1 recursive-call2)
+                 ;; E ::= liftB λ E E
+                 (liftB (choose (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none))
+                                (λ (clock location) (if (or (>= (time-vec->integer clock) 2130) (< (time-vec->integer clock) 800))
+                                 'night
+                                 (if (equal? location 'home)
+                                     'home
+                                     'away))))
+                        recursive-call1 recursive-call2)
+                 ;; E ::= arity-3-op E E E               
+                 ((choose ifE
+                          ifB
+                          ) recursive-call1 recursive-call2 recursive-call3)          
+                 )))
+
