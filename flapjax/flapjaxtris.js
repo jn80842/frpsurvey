4b0e266b9c94be1ec5e0d57bdc03704528788fd6
Julie Newcomb <newcombj@cs.washington.edu>
2017-09-05 17:02:22 -0700

    tinkering with flapjaxtris

--- flapjax/flapjaxtris.js
+++ flapjax/flapjaxtris.js
@@ -3,6 +3,14 @@ function updateTargetText(target,updateFunction) {
   target.innerHTML = updateFunction(text);
 }
 
+function setVisibility(target,bool) {
+  if (bool) {
+    target.style.visibility = "visible";
+  } else {
+    target.style.visibility = "hidden";
+  }
+}
+
 // need to look at what text was previously to get current state & condition on it
 function switchButtonState(text) {
   if (text == "New Game") {
@@ -16,10 +24,19 @@ function switchButtonState(text) {
   }
 }
 
+// draw piece
+function drawPiece() {
+
+}
+
 
 // initial state of the button is from the HTML
 var gameButtonElt = document.getElementById("fjt-button")
+var infoOverlay = document.getElementById("info-text");
 var buttonClickE = clicksE(gameButtonElt)
-.mapE(function() { updateTargetText(gameButtonElt,switchButtonState) });
-
+.mapE(function() { 
+  var currentState = gameButtonElt.innerHTML;
+  updateTargetText(gameButtonElt,switchButtonState);
+  setVisibility(infoOverlay,currentState == "Pause");
+});
 
