083dd47ac3f7c17142b35a84ec1d53f9ea792338
Julie Newcomb <newcombj@cs.washington.edu>
2017-10-01 23:08:43 -0700

    dense implementation of the event stream operators

---
+++ racketimpl/dense-fjmodels.rkt
@@ -0,0 +1,178 @@
+#lang rosette
+
+(require rosette/lib/synthax)
+(provide (all-defined-out))
+
+;(error-print-width 100000000000)
+
+(define (max-for-current-bitwidth n)
+  (sub1 (expt 2 (sub1 n))))
+
+(define (print-bitwidth-warning)
+  (printf "Current bitwidth is ~a; values larger than ~a will overflow.~n"
+          (current-bitwidth) (max-for-current-bitwidth (current-bitwidth))))
+
+(define (all-but-last lst)
+  (reverse (cdr (reverse lst))))
+
+;;;; represent times with vectors to allow us to use bitwidth 5 ;;;;
+(define (s-time-vec)
+  (define-symbolic* hour integer?)
+  (define-symbolic* minute-tens integer?)
+  (define-symbolic* minute-ones integer?)
+  (vector hour minute-tens minute-ones))
+
+(define (time-vec->integer vec)
+  (+ (* (vector-ref vec 0) 100) (* (vector-ref vec 1) 10) (vector-ref vec 2)))
+(define (integer->time-vec t)
+  (vector (quotient t 100) (quotient (remainder t 100) 10) (modulo t 10)))
+
+;; note: time-vecs don't need to be numerically sorted!
+;; an smaller time-vec following a larger one represents the next day
+;; while logical timesteps are monotonically increasing
+;; two equivalent time vecs in sequence is also legal, assumes logical time step is shorter than 1 min
+(define (valid-time-vec? vec)
+  (and (>= (vector-ref vec 0) 0)
+       (<= (vector-ref vec 0) 23)
+       (>= (vector-ref vec 1) 0)
+       (<= (vector-ref vec 1) 5)
+       (>= (vector-ref vec 2) 0)
+       (<= (vector-ref vec 2) 9)
+       ))
+
+;;;; note for all behavior operators:                       ;;;;
+;;;; behaviors *always* have a value (can never be 'no-evt) ;;;;
+
+(struct behavior (init changes) #:transparent)
+
+#;(define (valueNow behavior1 ts) ;; altered a bit for our own use
+  (let ([filtered-changes (filter (λ (t) (<= (get-timestamp t) ts)) (behavior-changes behavior1))])
+    (if (empty? filtered-changes)
+        (behavior-init behavior1)
+        (get-value (last filtered-changes)))))
+
+#;(define (valueNow behavior1 ts)
+  (cond [(empty? (behavior-changes behavior1)) (behavior-init behavior1)]
+        [(< ts (get-timestamp (first (behavior-changes behavior1)))) (behavior-init behavior1)]
+        [else (valueNow (behavior (get-value (first (behavior-changes behavior1))) (rest (behavior-changes behavior1))) ts)]))
+
+#;(define (valueNow-List l ts)
+  (let ([filtered-changes (filter (λ (t) (<= (get-timestamp t) ts)) l)])
+    (if (empty? filtered-changes)
+        0
+        (get-value (last filtered-changes)))))
+
+#;(define (project-values b ts)
+  (map (λ (t) (list t (valueNow b t))) ts))
+
+#;(define (project-values-list l ts)
+  (map (λ (t) (list t (valueNow-List l t))) ts))
+
+#;(define (projected-behavior b ts)
+  (behavior (behavior-init b) (project-values b ts)))
+
+#;(define (get-missing-timestamps b1 b2)
+  (filter (λ (t) (not (member t (map get-timestamp (behavior-changes b1)))))
+          (map get-timestamp (behavior-changes b2))))
+
+#;(define (valid-behavior? b)
+  (timestamps-sorted-and-distinct? (behavior-changes b)))
+
+#;(define (valid-time-vec-behavior? b)
+  (and (valid-behavior? b)
+       (behavior-check valid-time-vec-value? b)))
+
+#;(define (valid-time-vec-value? t)
+  (and (>= 23 (vector-ref t 0))
+       (>= (vector-ref t 0) 0)
+       (>= 5 (vector-ref t 1))
+       (>= (vector-ref t 1) 0)
+       (>= 9 (vector-ref t 2))
+       (>= (vector-ref t 2) 0)))
+
+#;(define (all-unique-timestamps . behaviors)
+  (sort (remove-duplicates (flatten (map (λ (b) (map get-timestamp (behavior-changes b))) behaviors))) <))
+
+#;(define (equal-behaviors? b1 b2)
+  (let ([enhanced-b1 (project-values b1 (all-unique-timestamps b1 b2))]
+        [enhanced-b2 (project-values b2 (all-unique-timestamps b2 b1))])
+    (and (equal? (behavior-init b1) (behavior-init b2))
+         (equal? enhanced-b1 enhanced-b2))))
+
+#;(define (behavior-check proc . behaviors)
+  (let ([all-ts (apply all-unique-timestamps behaviors)])
+    (and (apply proc (map behavior-init behaviors))
+         (andmap (λ (vs) (apply proc vs))
+                 (apply (curry map list) (map (λ (b) (map get-value (project-values b all-ts))) behaviors))))))
+
+(define (implication p q)
+  (or (not p) q))
+
+(define (empty-event? e)
+  (eq? 'no-evt e))
+(define (not-empty-event? e)
+  (not (eq? 'no-evt e)))
+
+;;;;; make symbolic event streams ;;;;;;;;;;
+
+(define (sym-boolean)
+  (define-symbolic* b boolean?)
+  b)
+
+(define (sym-integer)
+  (define-symbolic* i integer?)
+  i)
+
+(define (sym-union-constructor symbol1 symbol2)
+  (λ ()
+  (define-symbolic* b boolean?)
+  (if b symbol1 symbol2)))
+
+(define (sym-time-vec)
+  (define-symbolic* hour integer?)
+  (define-symbolic* minute-tens integer?)
+  (define-symbolic* minute-ones integer?)
+  (vector hour minute-tens minute-ones))
+
+(define (new-event-stream constructor n)
+  (for/list ([i n])
+    (define-symbolic* b boolean?)
+    (if b
+        (constructor)
+        'no-evt)))
+
+;;;;;;;; make symbolic behaviors ;;;;;;;;
+
+#;(define (new-behavior constructor n max-ts)
+  (behavior (constructor) (new-event-stream constructor n max-ts)))
+
+(define  (same program1 program2 . inputs)
+  (equal? (apply program1 inputs)
+                  (apply program2 inputs)))
+
+#;(define (harvest-term v)
+  (cond [(vector? v) (vector->list v)]
+        [(and (union? v) (eq? 2 (length (union-contents v)))) (car (first (union-contents v)))]
+        [(term? v) v]))
+
+#;(define (harvest-events evt-stream)
+  (flatten
+  (append (map get-timestamp evt-stream)
+          (map harvest-term (map get-value evt-stream)))))
+
+#;(define (harvest-behavior b)
+  (flatten (append (list (harvest-term (behavior-init b))) (harvest-events (behavior-changes b)))))
+
+#;(define (harvest x)
+  (if (behavior? x)
+      (harvest-behavior x)
+      (harvest-events x)))
+
+(define (check-existence-of-solution spec . inputs)
+  (displayln "checking assumptions ....")
+  (define solved (solve (assert (apply spec inputs))))
+  (if (unsat? solved)
+      (displayln "no solution for assumptions")
+      (begin (displayln "sample solution for assumptions:")
+             (map (λ (i) (displayln (evaluate i solved))) inputs)))
+  (void))
---
+++ racketimpl/densefjapi.rkt
@@ -0,0 +1,236 @@
+#lang rosette
+(provide (all-defined-out))
+
+(require "dense-fjmodels.rkt")
+
+;;;;;;;;; operator wishlist ;;;;;;
+;;
+;; highFreqE: propagate events that occur x seconds after prev
+;; collectE but where internal value and output value can be different
+;; mapE, collectE, liftB etc where the functions have access to timestamps
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;; flapjax API ;;;;;
+
+(define (oneE evt-stream)
+  (let ([val (findf not-empty-event? evt-stream)])
+    (if val (list val) '())))
+
+(define (zeroE)
+  '()) ;; a stream that never fires
+
+;; is it better to return e or no-evt if event is empty?
+(define (mapE proc evt-stream)
+  (map (λ (e) (if (empty-event? e) e (proc e))) evt-stream))
+
+(define (mergeE evt-stream1 evt-stream2)
+  (map (λ (evt1 evt2) (if (empty-event? evt2) evt1 evt2))
+       evt-stream1 evt-stream2))
+
+(define (switchE stream-of-streams)
+  (letrec ([f (λ (s-of-s current-stream)
+                (if (and (empty? s-of-s) (empty? current-stream))
+                    '()
+                    (let ([next-stream (first s-of-s)])
+                      (cond [(and (or (empty-event? next-stream) (empty? next-stream)) (empty? current-stream)) (append (list 'no-evt) (f (rest s-of-s) current-stream))]
+                            [(and (or (empty-event? next-stream) (empty? next-stream)) (not (empty? current-stream))) (append (list (first current-stream)) (f (rest s-of-s)
+                                                                                                                                     (rest current-stream)))]
+                            [else (append (list (first next-stream)) (f (rest s-of-s) (rest next-stream)))]))))])
+    (f stream-of-streams '())))
+
+
+#;(define (switchE stream-of-streams)
+  (if (empty? stream-of-streams)
+      '()
+      (let ([ts (map (λ (e) (get-timestamp e)) stream-of-streams)])
+        (apply append (filter (λ (l) (not (void? l)))
+               (map (λ (start-ts end-ts vals) (if end-ts
+                                           (boundedTimestampsStream start-ts end-ts (get-value vals))
+                                           (startAtTimestamp start-ts (get-value vals))))
+             ts (append (rest ts) (list #f)) stream-of-streams))))))
+
+;; condE
+
+(define (filterE pred stream)
+  (filter (λ (e) (if (and (not-empty-event? e) (pred e)) e 'no-evt)) stream))
+
+(define (ifE guard-stream true-stream false-stream)
+  (map (λ (guard true false) (if (empty-event? guard) 'no-evt (if guard true false)))
+       guard-stream true-stream false-stream))
+
+(define (constantE const evt-stream)
+  (map (λ (x) (if (empty-event? x) 'no-evt const)) evt-stream))
+
+(define (collectE init proc evt-stream)
+  (letrec ([collect (λ (x-lst prev)
+                      (if (empty? x-lst)
+                          '()
+                          (let ([evt (first x-lst)])
+                            (if (empty-event? evt)
+                                (append (list 'no-evt) (collect (rest x-lst) prev))
+                                (append (list (proc evt prev) (collect (rest x-lst) prev)))))))])
+    (collect evt-stream init)))
+
+;; andE
+
+;; orE
+
+(define (notE evt-stream)
+  (map (λ (e) (if (empty-event? e) e (not e))) evt-stream))
+
+;; filterRepeatsE
+
+;; send/receive
+
+#;(define (snapshotE evt-stream behavior1)
+  (let ([real-evt-stream (filter (λ (e) (not (eq? (get-value e) 'no-evt))) evt-stream)])
+    (map (λ (t) (list (get-timestamp t) (valueNow behavior1 (get-timestamp t)))) real-evt-stream)))
+
+;; onceE
+
+;; skipFirstE
+
+(define (delayE interval evt-stream)
+  (append (for/list ([i interval]) 'no-evt) evt-stream))
+
+(define (blindE interval evt-stream)
+  (letrec ([f (λ (evts last-sent)
+                (cond [(empty? evts) '()]
+                      [(empty-event? (first evts)) (append (list 'no-evt) (f (rest evts) (add1 last-sent)))]
+                      [(>= last-sent interval) (append (list (first evts)) (f (rest evts) 0))]
+                      [else (append (list 'no-evt) (f (rest evts) (add1 last-sent)))]))])
+    (f evt-stream 0)))
+
+(define (calmE interval evt-stream)
+  (letrec ([f (λ (evts last-sent buffered-evt)
+                (cond [(and (empty? evts) (not buffered-evt)) '()]
+                      [(and (empty? evts) (>= last-sent interval)) (list buffered-evt)]
+                      [(empty? evts) (append (list 'no-evt) (f evts (add1 last-sent) buffered-evt))]
+                      [else (let ([output (if (>= last-sent interval) buffered-evt 'no-evt)]
+                                  [new-last-sent (cond [(and (not-empty-event? (first evts))
+                                                          (>= last-sent interval)) 0]
+                                                       [(< last-sent interval) (add1 last-sent)]
+                                                       [else last-sent])])
+                              (append (list output) (f (rest evts) (first evts) new-last-sent)))]))])
+    (f evt-stream 0 'no-evt)0))
+
+#;(define (calmE interval evt-stream)
+  (letrec ([emit-event (λ (evt) (list (list (+ (get-timestamp evt) interval) (get-value evt))))]
+           [f (λ (evts buffered-evt)
+                ;; if all events have been processed, propagate buffered event
+                (cond [(and (empty? evts) buffered-evt) (emit-event buffered-evt)]
+                      ;; if all events have been processed and no event is buffered, done!
+                      [(empty? evts) '()]
+                      ;; if nothing has been buffered, buffer event and continue
+                      [(false? buffered-evt) (f (rest evts) (first evts))]
+                      ;; if time between buffered event and next event is geq interval
+                      ;; propagate buffered event and continue
+                      [(<= interval (- (get-timestamp (first evts)) (get-timestamp buffered-evt)))
+                       (append (emit-event buffered-evt) (f (rest evts) (first evts)))]
+                      ;; if time between buffered event and next event is too small
+                      ;; discard buffered event and continue
+                      [else (f (rest evts) (first evts))]))])
+    (f evt-stream #f)))
+
+#;(define (startsWith init-value evt-stream)
+  (behavior init-value evt-stream))
+
+#;(define (changes behaviorB)
+    (behavior-changes behaviorB))
+
+#;(define (constantB const)
+  (behavior const '()))
+
+#;(define (delayB interval behavior1)
+  (behavior (behavior-init behavior1) (delayE interval (behavior-changes behavior1))))
+
+;; valueNow: since valueNow shouldn't be exposed to end users, it's in fjmodels.rkt
+
+;; switchB
+;; switchBB takes a behavior of behaviors: (behavior behavior1 (list behavior2 behavior3 behavior4)))
+;; and returns a behavior: (behavior (behavior-init behavior1) (append (behavior-changes behavior1) (behavior-changes behavior2) ...)))
+
+#;(define (andB behavior1 behavior2)
+  (let* ([unique-ts (sort (remove-duplicates (append (map get-timestamp (behavior-changes behavior1))
+                                                     (map get-timestamp (behavior-changes behavior2)))) <)]
+         [enhanced-b1 (project-values behavior1 unique-ts)]
+         [enhanced-b2 (project-values behavior2 unique-ts)])
+    (behavior (and (behavior-init behavior1) (behavior-init behavior2))
+              (map (λ (b1 b2) (list (get-timestamp b1) (and (get-value b1) (get-value b2)))) enhanced-b1 enhanced-b2)))
+  )
+
+#;(define (orB behavior1 behavior2)
+  (let* ([unique-ts (sort (remove-duplicates (append (map get-timestamp (behavior-changes behavior1))
+                                                     (map get-timestamp (behavior-changes behavior2)))) <)]
+         [enhanced-b1 (project-values behavior1 unique-ts)]
+         [enhanced-b2 (project-values behavior2 unique-ts)])
+    (behavior (or (behavior-init behavior1) (behavior-init behavior2))
+              (map (λ (b1 b2) (list (get-timestamp b1) (or (get-value b1) (get-value b2)))) enhanced-b1 enhanced-b2))))
+
+#;(define (notB behavior1)
+  (behavior (not (behavior-init behavior1)) (notE (behavior-changes behavior1))))
+
+#;(define (liftB proc . argBs)
+  (let* ([unique-ts (sort (remove-duplicates (flatten (map (λ (b) (map get-timestamp (behavior-changes b))) argBs))) <)]
+         [enhanced-argBs (map (λ (b) (project-values b unique-ts)) argBs)])
+  (behavior (apply proc (map behavior-init argBs)) (apply (curry map (λ e (list (get-timestamp (first e)) (apply proc (map get-value e))))) enhanced-argBs))))
+
+#;(define (liftB1 proc argB)
+  (behavior (proc (behavior-init argB)) (map (λ (b) (list (get-timestamp b) (proc (get-value b)))) (behavior-changes argB))))
+
+#;(define (liftB2 proc argB1 argB2)
+  (behavior (proc (behavior-init argB1) (behavior-init argB2))
+            (letrec ([f (λ (b1 b2) (cond [(or (empty? (behavior-changes b1))
+                                              (empty? (behavior-changes b2))) '()]
+                                         [(eq? (get-timestamp (first (behavior-changes b1)))
+                                               (get-timestamp (first (behavior-changes b2))))
+                                          (append (list (list (get-timestamp (first (behavior-changes b1)))
+                                                              (proc (get-value (first (behavior-changes b1))) (get-value (first (behavior-changes b2))))))
+                                                  (f (behavior (get-value (first (behavior-changes b1))) (rest (behavior-changes b1)))
+                                                     (behavior (get-value (first (behavior-changes b2))) (rest (behavior-changes b2)))))]
+                                         [(< (get-timestamp (first (behavior-changes b1))) (get-timestamp (first (behavior-changes b2))))
+                                          (append (list (list (get-timestamp (first (behavior-changes b1)))
+                                                              (proc (get-value (first (behavior-changes b1))) (valueNow b2 (get-timestamp (first (behavior-changes b1)))))))
+                                                        (f (behavior (get-value (first (behavior-changes b1))) (rest (behavior-changes b1))) b2))]
+                                         [else (append (list (list (get-timestamp (first (behavior-changes b2)))
+                                                                   (proc (valueNow b1 (get-timestamp (first (behavior-changes b2)))) (get-value (first (behavior-changes b2))))))
+                                                       (f b1 (behavior (get-value (first (behavior-changes b2))) (rest (behavior-changes b2)))))]))])
+              (f argB1 argB2))))
+
+;; is there an easier way??
+#;(define (condB behaviorpairs)
+  (let* ([all-bool-ts (flatten (map (λ (b) (map get-timestamp (behavior-changes (first b)))) behaviorpairs))] ;; every timestamp mentioned in any boolean behavior
+         [all-val-ts (flatten (map (λ (b) (map get-timestamp (behavior-changes (second b)))) behaviorpairs))] ;; every timestamp mentioned in any value behavior
+         [unique-ts (sort (remove-duplicates (append all-bool-ts all-val-ts)) <)] ;; sorted list of all unique timestamps
+         [enhanced-behaviorpairs (map (λ (bp) (list (behavior (behavior-init (first bp)) (project-values (first bp) unique-ts)) ;; every behavior with every timestamp made explicit
+                                                    (behavior (behavior-init (second bp)) (project-values (second bp) unique-ts)))) behaviorpairs)]
+         [fused-pairs (map (λ (bp) (behavior (list (behavior-init (first bp)) (behavior-init (second bp)))
+                                             (map (λ (b1 b2) (list (get-timestamp b1) (list (get-value b1) (get-value b2))))
+                                                  (behavior-changes (first bp)) (behavior-changes (second bp))))) enhanced-behaviorpairs)]
+         [guarded-init (ormap (λ (b) (if (first (behavior-init b)) (behavior-init b) #f)) fused-pairs)]
+         [final-init (if (first guarded-init) (second guarded-init) #f)]
+         [final-changes (map (λ (tsitems) (list (get-timestamp (first tsitems))
+                                                (ormap (λ (item) (if (first (get-value item)) (get-value item) #f)) tsitems)))
+                             (apply (curry map list) (map behavior-changes fused-pairs)))]
+         )
+    (behavior final-init (map (λ (c) (if (get-value c) (list (get-timestamp c) (second (get-value c))) c)) final-changes))))
+
+#;(define (ifB conditionB trueB falseB)
+  (let* ([unique-ts (sort (remove-duplicates (append (map get-timestamp (behavior-changes conditionB))
+                                                     (map get-timestamp (behavior-changes trueB))
+                                                     (map get-timestamp(behavior-changes falseB)))) <)]
+        [enhanced-condB (project-values conditionB unique-ts)]
+        [enhanced-trueB (project-values trueB unique-ts)]
+        [enhanced-falseB (project-values falseB unique-ts)])
+  (behavior (if (behavior-init conditionB) (behavior-init trueB) (behavior-init falseB))
+            (map (λ (c t f) (list (get-timestamp c) (if (get-value c) (get-value t) (get-value f)))) enhanced-condB enhanced-trueB enhanced-falseB))))
+
+;; timerB
+
+#;(define (blindB interval b)
+  (behavior (behavior-init b) (blindE interval (changes b))))
+
+#;(define (calmB interval b)
+  (behavior (behavior-init b) (calmE interval (changes b))))
+
