d118f2aea988a73518945585a151babfac4a45b3
Julie Newcomb <newcombj@cs.washington.edu>
2017-10-10 23:28:13 -0700

    dense kitchen light benchmark

--- racketimpl/benchmarks/kitchenlight.rkt
+++ racketimpl/benchmarks/kitchenlight.rkt
@@ -1,32 +1,70 @@
 #lang rosette/safe
 (require rosette/lib/synthax)
 
-(require "../fjmodels.rkt")
-(require "../rosettefjapi.rkt")
+(require "../dense-fjmodels.rkt")
+(require "../densefjapi.rkt")
 
 (provide (all-defined-out))
 
-(define stream-length 6)
+(current-bitwidth 5)
+(define stream-length 2)
 
 (define (sym-location)
   (define-symbolic* b boolean?)
   (if b 'home 'not-at-home))
 
-(define s-motion-sensorB (new-behavior sym-boolean stream-length (* 2 stream-length)))
-(define s-locationB (new-behavior sym-location stream-length (* 2 stream-length)))
-(define s-clockB (new-behavior sym-time-vec stream-length (* 2 stream-length)))
+(define concrete-motion-sensorB (behavior #f '(#t #f)))
+(define concrete-clockB (behavior 0 '(1 2)))
+(define concrete-location (behavior 'home '(not-at-home home)))
 
-(define (mode-assumptions clockB locationB)
-  (and (valid-behavior? clockB)
+;; use fake hour constants for smaller bitwidths
+(define hour-begin 4)
+(define hour-end 2)
+
+(define s-motion-sensorB (new-behavior sym-boolean stream-length))
+(define s-locationB (new-behavior sym-location stream-length))
+;(define s-clockB (new-behavior sym-time-vec stream-length))
+(define s-clockB (new-behavior sym-integer stream-length))
+
+#;(define (mode-assumptions clockB locationB)
+  (and; (valid-behavior? clockB)
        (valid-time-vec? (behavior-init clockB))
-       (andmap (λ (e) (valid-time-vec? (get-value e))) (behavior-changes clockB))
-       (valid-behavior? locationB)
+       (andmap (λ (e) (valid-time-vec? e)) (behavior-changes clockB))
+     ;  (valid-behavior? locationB)
        ))
 
-(define (light-status-assumptions motionSensorB)
-  (and (valid-behavior? motionSensorB)))
+#;(define (light-status-assumptions motionSensorB)
+  #t) ;(and (valid-behavior? motionSensorB)))
 
-(define (light-color-assumptions clockB locationB motionSensorB)
+#;(define (light-color-assumptions clockB locationB motionSensorB)
   (and (mode-assumptions clockB locationB)
        (light-status-assumptions motionSensorB)))
 
+;; between 21:30 and 8:00 inclusive, mode is night
+;; otherwise, if user is at home, home, else away
+(define (mode-graph clockB userLocationB motionSensorB)
+  (liftB2 (λ (clock location) (if (or (>= clock hour-begin) (< clock hour-end))
+                                 'night
+                                 (if (equal? location 'home)
+                                     'home
+                                     'away)))
+         clockB userLocationB))
+
+;; when motion sensor senses motion, light turns on
+(define (kitchen-light-status-graph clockB userLocationB motionSensorB)
+  (liftB1 (λ (e) (if e 'on 'off)) motionSensorB))
+
+;; when light turns on, if mode is night, light is orange, else white
+(define (kitchen-light-color-graph kitchenLightB modeB)
+  (liftB2 (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none)) kitchenLightB modeB))
+
+;; full graph
+(define (kitchen-light-graph clockB userLocationB motionSensorB)
+  (liftB2 (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none))
+         (liftB1 (λ (e) (if e 'on 'off)) motionSensorB)
+         (liftB2 (λ (clock location) (if (or (>= clock hour-begin) (< clock hour-end))
+                                 'night
+                                 (if (equal? location 'home)
+                                     'home
+                                     'away)))
+         clockB userLocationB)))
--- racketimpl/synthesis/kitchenlight.rkt
+++ racketimpl/synthesis/kitchenlight.rkt
@@ -1,8 +1,8 @@
 #lang rosette/safe
 (require rosette/lib/synthax)
 
-(require "../fjmodels.rkt")
-(require "../rosettefjapi.rkt")
+(require "../dense-fjmodels.rkt")
+(require "../densefjapi.rkt")
 (require "../benchmarks/kitchenlight.rkt")
 
 ;;            liftB
@@ -13,91 +13,27 @@
 
 (define-synthax (flapjax-grmr input ... depth)
   #:base (choose input ...)
-  #:else (choose input ...
-                 ;; zero arity
-                ; (zeroE)
-                ; (constantB (choose 'on 'off (??)))
-                 ;; E ::= arity-1-op E
-                ; ((choose oneE
-                ;   switchE
-                ;   notE
-                ;   changes
-                ;   ) (flapjax-grmr input ... (sub1 depth)))
-                 ;; E ::= arity-1-op val E
-                ; ((choose
-                 ;  startsWith
-                 ;  constantE
-                 ;  delayE
-                 ;  blindE
-                 ;  calmE
-                 ;  mapE
-                 ;  filterE
-                 ;  liftB
-                 ;  )
-                 ; (choose ;(??)
-                 ;         (λ (e) (if e 'on 'off))
-                 ;         (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none))
-                         ; (λ (t) (<= t (??)))
-                 ;         (λ (c) (or (>= (vector-ref c 0) (??)) (>= (??) (vector-ref c 0))))
-                         ; (λ (e) (list (get-timestamp e) (+ (get-value e) (??))))
-                 ;         )
-                 ; (flapjax-grmr input ... (sub1 depth)))
-                 ;; E ::= collectE init λ E
-                ; (collectE (??) + (flapjax-grmr input ... (sub1 depth)))
-                 ;; E ::= arity-2-op E E
-                ; ((choose andB
-                ;          mergeE
-                ;          snapshotE
-                ;          ) (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)))
-                 ;; E ::= liftB λ E E
-                 (liftB ;(choose (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none))
-                                (λ (clock location) (if (or (>= (vector-ref clock 0) 21) (< (vector-ref clock 0) 8))
-                                 'night
-                                 (if (equal? location 'home)
-                                     'home
-                                     'away))) ;)
-                        (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)))
-                 ;; E ::= arity-3-op E E E
-                 ;((choose ifE
-                  ;        ifB
-                   ;       ) (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)) (flapjax-grmr input ... (sub1 depth)))
-                 ))
-
-;; between 21:30 and 8:00 inclusive, mode is night
-;; otherwise, if user is at home, home, else away
-(define (mode-graph clockB userLocationB) ;motionSensorB)
-  (liftB (λ (clock location) (if (or (>= (time-vec->integer clock) 2130) (< (time-vec->integer clock) 800))
+  #:else (let ([recursive1 (flapjax-grmr input ... (sub1 depth))]
+               [recursive2 (flapjax-grmr input ... (sub1 depth))])
+           (choose input ...
+                   (liftB1 (λ (e) (if e 'on 'off)) (flapjax-grmr input ... (sub1 depth)))
+                   (liftB2 (choose (λ (clock location) (if (or (>= clock hour-begin) (< clock hour-end))
                                  'night
                                  (if (equal? location 'home)
                                      'home
                                      'away)))
-         clockB userLocationB))
+                                   (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none)))
+                           recursive1 recursive2)
+                 )))
 
-;; when motion sensor senses motion, light turns on
-(define (kitchen-light-status-graph clockB userLocationB motionSensorB)
-  (liftB (λ (e) (if e 'on 'off)) motionSensorB))
 
-;; when light turns on, if mode is night, light is orange, else white
-(define (kitchen-light-color-graph kitchenLightB modeB)
-  (liftB (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none)) kitchenLightB modeB))
 
-;; full graph
-(define (kitchen-light-graph clockB userLocationB motionSensorB)
-  (liftB (λ (light mode) (if (equal? light 'on) (if (equal? mode 'night) 'orange 'white) 'none))
-         (liftB (λ (e) (if e 'on 'off)) motionSensorB)
-         (liftB (λ (clock location) (if (or (>= (time-vec->integer clock) 2130) (< (time-vec->integer clock) 800))
-                                 'night
-                                 (if (equal? location 'home)
-                                     'home
-                                     'away)))
-         clockB userLocationB)))
+(define (synth-graph clockB userLocationB motionSensorB)
+  (flapjax-grmr clockB userLocationB motionSensorB 3))
 
-(define (synth-graph clockB userLocationB) ; motionSensorB)
-  (flapjax-grmr clockB userLocationB 3))
-
-(assert (light-color-assumptions s-clockB s-locationB s-motionSensorB))
+;(assert (light-color-assumptions s-clockB s-locationB s-motion-sensorB))
 ;; limit range of timestamps, lock down minutes
-(assert (and (andmap (λ (ts) (>= (* 4 stream-length) ts)) (append (map get-timestamp (behavior-changes s-clockB))
+#;(assert (and (andmap (λ (ts) (>= (* 4 stream-length) ts)) (append (map get-timestamp (behavior-changes s-clockB))
                                                                   (map get-timestamp (behavior-changes s-locationB))
                                                                   (map get-timestamp (behavior-changes s-motionSensorB))))
              (behavior-check (λ (v) (and (= (vector-ref v 1) 0) (= (vector-ref v 2) 0))) s-clockB)))
@@ -105,16 +41,15 @@
 (displayln "Synthesize kitchen light program")
 
 (define begin-time (current-seconds))
-#;(define binding
-  (synthesize #:forall (append
-                        (harvest-behavior s-clockB)
-                               (harvest-behavior s-locationB)
-                               (harvest-behavior s-motionSensorB))
+(define binding
+  (synthesize #:forall (append (harvest s-clockB)
+                               (harvest s-locationB)
+                               (harvest s-motion-sensorB))
               #:guarantee (assert ;(same kitchen-light-graph synth-graph s-clockB s-locationB s-motionSensorB)
-                           (same mode-graph synth-graph s-clockB s-locationB) ;s-motionSensorB)
+                           (same kitchen-light-graph synth-graph s-clockB s-locationB s-motion-sensorB)
                                   )))
 (define end-time (current-seconds))
-#;(if (unsat? binding)
+(if (unsat? binding)
     (displayln "No binding was found.")
     (print-forms binding))
 (printf "Took ~a seconds~n" (- end-time begin-time))
