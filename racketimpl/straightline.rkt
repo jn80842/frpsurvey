115ee4dbe6f4c506d8098ce4f9f5649d4e9cd93e
Julie Newcomb <newcombj@cs.washington.edu>
2017-12-12 14:43:38 -0800

    start to organize straightline code

---
+++ racketimpl/straightline.rkt
@@ -0,0 +1,33 @@
+#lang rosette
+(require rosette/lib/synthax)
+
+(provide (all-defined-out))
+
+(define-namespace-anchor anc)
+(define ns (namespace-anchor->namespace anc))
+;; top level definitions of SSA variables
+;; hack to make eval of rewrites work
+(define r1 'r1)
+(define r2 'r2)
+(define r3 'r3)
+(define r4 'r4)
+(define r5 'r5)
+(define r6 'r6)
+
+(define (insn-printer insn-stx)
+  (eval (syntax-case insn-stx ()
+          [(define r1 ((curry op ARG) INPUT-STREAM))
+          #'(format "  (define ~a (~a ~a ~a))" 'r1 'op ARG INPUT-STREAM)]
+          [(define r1 ((curry collectE int-arg op-arg) INPUT-STREAM))
+          #'(format "  (define ~a (collectE ~a ~a ~a))" 'r1 'int-arg 'op-arg INPUT-STREAM)]
+          [(define r1 v1) #'(format "  (define ~a ~a)" 'r1 'v1)]
+          [retval #'(format "  ~a)" 'retval)]
+          [_ #'(format "no match")]) ns))
+
+(define (function-printer binding)
+  (let ([syntax-lst (syntax-e (list-ref (generate-forms binding) 0))])
+    (begin
+      (displayln (format "(~a ~a" (syntax->datum (list-ref syntax-lst 0))
+                         (syntax->datum (list-ref syntax-lst 1))))
+      (for ([s (list-tail syntax-lst 2)])
+        (displayln (insn-printer s))))))
--- racketimpl/straightline_synthesis/incdecbutton.rkt
+++ racketimpl/straightline_synthesis/incdecbutton.rkt
@@ -1,13 +1,10 @@
 #lang rosette
-(require rosette/lib/angelic)
 (require rosette/lib/synthax)
 
 (require "../dense-fjmodels.rkt")
 (require "../densefjapi.rkt")
+(require "../straightline.rkt")
 (require "../benchmarks/incdecbutton.rkt")
-;;(require "grammar.rkt")
-
-(provide (all-defined-out))
 
 (define (straightline-graph inc dec)
   (define r1 inc)
@@ -81,47 +78,6 @@
                       (curry startsWith (??))) (list-ref (list r1 r2 r3 r4 r5 r6) (choose 0 1 2 3 4 5))))
   r7)
 
-
-(define-syntax (macro-graph stx)
-  (syntax-case stx ()
-    [(_ n name arg1 arg2)
-     (with-syntax ([main-body (datum->syntax stx
-                                             (string->symbol (apply string-append
-                                                                    (for/list ([i (in-range 0 (syntax->datum #'n))])
-                                                                      (format "(+ ~a ~a)"
-                                                                              (syntax->datum #'arg1)
-                                                                              (syntax->datum #'arg2))))))])
-       #'(define (name arg1 arg2)
-           main-body))]))
-
-(define-namespace-anchor anc)
-(define ns (namespace-anchor->namespace anc))
-;; top level definitions of SSA variables
-;; hack to make eval of rewrites work
-(define r1 'r1)
-(define r2 'r2)
-(define r3 'r3)
-(define r4 'r4)
-(define r5 'r5)
-(define r6 'r6)
-
-(define (insn-printer insn-stx)
-  (eval (syntax-case insn-stx ()
-          [(define r1 ((curry op ARG) INPUT-STREAM))
-          #'(format "  (define ~a (~a ~a ~a))" 'r1 'op ARG INPUT-STREAM)]
-          [(define r1 ((curry collectE int-arg op-arg) INPUT-STREAM))
-          #'(format "  (define ~a (collectE ~a ~a ~a))" 'r1 'int-arg 'op-arg INPUT-STREAM)]
-          [(define r1 v1) #'(format "  (define ~a ~a)" 'r1 'v1)]
-          [retval #'(format "  ~a)" 'retval)]
-          [_ #'(format "no match")]) ns))
-
-(define (function-printer binding)
-  (let ([syntax-lst (syntax-e (list-ref (generate-forms binding) 0))])
-    (begin
-      (displayln (format "(~a ~a" (syntax->datum (list-ref syntax-lst 0))
-                         (syntax->datum (list-ref syntax-lst 1))))
-      (for ([s (list-tail syntax-lst 2)])
-        (displayln (insn-printer s))))))
 
 (define binding
   (time (synthesize #:forall (append (harvest s-inc) (harvest s-dec))
