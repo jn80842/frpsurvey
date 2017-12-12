82f3b686899a47fb1d2dc246c89a3324d37bb5a8
Julie Newcomb <newcombj@cs.washington.edu>
2017-12-11 16:45:26 -0800

    remove unnecessary things from straightline

--- racketimpl/synthesis/straightline.rkt
+++ racketimpl/synthesis/straightline.rkt
@@ -2,19 +2,14 @@
 (require rosette/lib/angelic)
 (require rosette/lib/synthax)
 
-;;(require "../dense-fjmodels.rkt")
+(require "../dense-fjmodels.rkt")
 (require "../densefjapi.rkt")
-;;(require "../benchmarks/incdecbutton.rkt")
+(require "../benchmarks/incdecbutton.rkt")
 ;;(require "grammar.rkt")
 
 (provide (all-defined-out))
 
-#;(define (inc-dec-button-graph inc dec)
-  (startsWith 0
-   (collectE 0 +
-   (mergeE (constantE 1 inc) (constantE -1 dec)))))
-
-#;(define (straightline-graph inc dec)
+(define (straightline-graph inc dec)
   (define r1 inc)
   (define r2 dec)
   (define r3 (constantE 1 r1))
@@ -24,104 +19,43 @@
   (define r7 (startsWith 0 r6))
   r7)
 
-#;(define-syntax-rule (operator-choose regs)
-  ((choose* (curry constantE (choose* -1 0 1 2 3 4))
-                      (curry mergeE (list-ref regs (choose* 0 1 2 3 4)))
-                      (curry collectE (choose* -1 0 1 2 3 4) + )
-                      (curry startsWith (choose* -1 0 1 2 3 4))) (list-ref regs (choose* 0 1 2 3 4))))
-
-(define (synth-insn regs)
-  ((choose* (curry constantE (choose* -1 0 1 2 3 4))
-            (curry mergeE (list-ref regs (choose* 0 1 2 3 4)))
-            (curry collectE (choose* -1 0 1 2 3 4) +)
-            (curry startsWith (choose* -1 0 1 2 3 4)))
-   (list-ref regs (choose* 0 1 2 3 4))))
-
-#;(define (one-arg-2-depth-graph arg1)
-  (define r1 arg1)
-  (define r2 ((choose (curry collectE (??) +)
-                      (curry constantE (??))
-                      (curry delayE (??))
-                      (curry mapE (λ (e) (+ e (??))))
-                      (curry startsWith (??))) r1))
-  r2)
-
-(define (one-arg-2-depth-graph arg1)
-  (define r1 arg1)
-  (define r2 (delayE (??) r1))
-  r2)
-
-(define (one-arg-3-depth-graph arg1)
-  (define r1 arg1)
-  (define r2 ((choose (curry collectE (??) +)
-                      (curry constantE (??))
-                      (curry delayE (??))
-                      (curry mapE (λ (e) (+ e (??))))
-                      (curry startsWith (??))) r1))
-  (define r3 ((choose (curry collectE (??) +)
-                      (curry constantE (??))
-                      (curry delayE (??))
-                      (curry mapE (λ (e) (+ e (??))))
-                      (curry startsWith (??))) (list-ref (list r1 r2) (choose 0 1))))
-  r3)
-
 (define (fully-expanded-sketch-graph inc dec)
   (define r1 inc)
   (define r2 dec)
-  (define r3 ((choose identityE
-                      (curry constantE (??))
-                      (curry delayE (??))
-                      (curry mapE (λ (e) (+ e (??))))
-                      (curry mergeE (list-ref (list r1 r2) (choose 0 1)))
-                      notE
-                      (curry collectE (??) + )
-                      (curry startsWith (??))) (list-ref (list r1 r2) (choose 0 1))))
-  (define r4 ((choose identityE
-                      (curry constantE (??))
-                      (curry delayE (??))
-                      (curry mapE (λ (e) (+ e (??))))
-                      (curry mergeE (list-ref (list r1 r2 r3) (choose 0 1 2)))
-                      notE
-                      (curry collectE (??) + )
-                      (curry startsWith (??))) (list-ref (list r1 r2 r3) (choose 0 1 2))))
-  (define r5 ((choose identityE
-                      (curry constantE (??))
-                      (curry delayE (??))
-                      (curry mapE (λ (e) (+ e (??))))
-                      (curry mergeE (list-ref (list r1 r2 r3 r4) (choose 0 1 2 3)))
-                      notE
-                      (curry collectE (??) + )
-                      (curry startsWith (??))) (list-ref (list r1 r2 r3 r4) (choose 0 1 2 3))))
-  (define r6 ((choose identityE
-                      (curry constantE (??))
-                      (curry delayE (??))
-                      (curry mapE (λ (e) (+ e (??))))
-                      (curry mergeE (list-ref (list r1 r2 r3 r4 r5) (choose 0 1 2 3 4)))
-                      notE
-                      (curry collectE (??) + )
-                      (curry startsWith (??))) (list-ref (list r1 r2 r3 r4 r5) (choose 0 1 2 3 4))))
-  (define r7 ((choose identityE
-                      (curry constantE (??))
-                      (curry delayE (??))
-                      (curry mapE (λ (e) (+ e (??))))
-                      (curry mergeE (list-ref (list r1 r2 r3 r4 r5 r6) (choose 0 1 2 3 4 5)))
-                      notE
-                      (curry collectE (??) + )
-                      (curry startsWith (??))) (list-ref (list r1 r2 r3 r4 r5 r6) (choose 0 1 2 3 4 5))))
+  (define r3 ((choose (curry constantE (??))
+                       (curry mergeE (list-ref (list r1 r2) (choose 0 1)))
+                       (curry collectE (??) + )
+                       (curry startsWith (??))) (list-ref (list r1 r2) (choose 0 1))))
+  (define r4 ((choose (curry constantE (??))
+                       (curry mergeE (list-ref (list r1 r2 r3) (choose 0 1 2)))
+                       (curry collectE (??) + )
+                       (curry startsWith (??))) (list-ref (list r1 r2 r3) (choose 0 1 2))))
+  (define r5 ((choose (curry constantE (??))
+                       (curry mergeE (list-ref (list r1 r2 r3 r4) (choose 0 1 2 3)))
+                       (curry collectE (??) + )
+                       (curry startsWith (??))) (list-ref (list r1 r2 r3 r4) (choose 0 1 2 3))))
+  (define r6 ((choose (curry constantE (??))
+                       (curry mergeE (list-ref (list r1 r2 r3 r4 r5) (choose 0 1 2 3 4)))
+                       (curry collectE (??) + )
+                       (curry startsWith (??))) (list-ref (list r1 r2 r3 r4 r5) (choose 0 1 2 3 4))))
+  (define r7 ((choose (curry constantE (??))
+                       (curry mergeE (list-ref (list r1 r2 r3 r4 r5 r6) (choose 0 1 2 3 4 5)))
+                       (curry collectE (??) + )
+                       (curry startsWith (??))) (list-ref (list r1 r2 r3 r4 r5 r6) (choose 0 1 2 3 4 5))))
   r7)
+
 
 (define-syntax (macro-graph stx)
   (syntax-case stx ()
-    ([_ n arg1 arg2]
-     (with-syntax ([choose-insn #'((choose (curry constantE (??))
-                       (curry mergeE (list-ref (list r1 r2) (choose 0 1)))
-                       (curry collectE (??) + )
-                       (curry startsWith (??))) (list-ref (list r1 r2) (choose 0 1)))])
-       #'(define (sketch-graph arg1 arg2)
-           (define r1 arg1)
-           (define r2 arg2)
-           (for ([i (in-range 0 n)])
-             (define r3 choose-insn)))))))
+    [(_ n name arg1 arg2)
+     (with-syntax ([main-body (datum->syntax stx
+                                             (string->symbol (apply string-append
+                                                                    (for/list ([i (in-range 0 (syntax->datum #'n))])
+                                                                      (format "(+ ~a ~a)"
+                                                                              (syntax->datum #'arg1)
+                                                                              (syntax->datum #'arg2))))))])
+       #'(define (name arg1 arg2)
+           main-body))]))
 
 (define-namespace-anchor anc)
 (define ns (namespace-anchor->namespace anc))
@@ -152,28 +86,11 @@
       (for ([s (list-tail syntax-lst 2)])
         (displayln (insn-printer s))))))
 
-(define (sketch-graph inc dec)
-  (define r1 inc)
-  (define r2 dec)
-  (define r3 (synth-insn (list r1 r2)))
-  (define r4 ((choose (curry constantE (choose* -1 0 1 2 3 4))
-            (curry mergeE (list-ref (list r1 r2 r3) (choose 0 1 2 3 4)))
-            (curry collectE (choose -1 0 1 2 3 4) +)
-            (curry startsWith (choose -1 0 1 2 3 4)))
-   (list-ref (list r1 r2 r3) (choose 0 1 2 3 4))))
-  (define r5 (mergeE r3 r4))
-  (define r6 (collectE 0 + r5))
-  (define r7 (startsWith 0 r6))
-  r7)
-
-
-;;(define verified (verify #:guarantee (same straightline-graph inc-dec-button-graph s-inc s-dec)))
-
-#;(define binding
+(define binding
   (time (synthesize #:forall (append (harvest s-inc) (harvest s-dec))
                     #:guarantee (assert (same inc-dec-button-graph
                                               fully-expanded-sketch-graph
                                               s-inc s-dec)))))
 
 ;;(print-forms binding)
-#;(function-printer binding)
+(function-printer binding)
