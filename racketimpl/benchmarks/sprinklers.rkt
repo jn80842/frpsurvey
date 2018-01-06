ddd954974b04a84ce8e86c45c6c257bb64b21b95
Julie Newcomb <newcombj@cs.washington.edu>
2018-01-05 16:08:49 -0800

    broken straightline sprinklers program

--- racketimpl/benchmarks/sprinklers.rkt
+++ racketimpl/benchmarks/sprinklers.rkt
@@ -1,7 +1,7 @@
 #lang rosette/safe
 
-(require "../rosettefjapi.rkt")
-(require "../fjmodels.rkt")
+(require "../densefjapi.rkt")
+(require "../dense-fjmodels.rkt")
 
 (provide (all-defined-out))
 
@@ -9,13 +9,52 @@
 ;;            /                   |                      \
 ;;          pair                 pair                     pair
 ;;     /           \            /    \                   /    \
-;;  motionSensorB  constantB  liftB   constantB    constantB    constantB
-;;                     |       /    \      |            |            |
-;;                    'off    λ  counterB  'on          #t          'off
+;;  motionSensorB  constantB  liftB2   constantB    constantB    constantB
+;;                     |     /   |   \       |         |            |
+;;                  'off   λ collectB clockB   'on       #t          'off
+;;                            /  |   \
+;;                          λ   #f   liftB2
+;;                                 /    |    \
+;;                              λ  raingaugeB clockB
 
 (current-bitwidth 5)
 (define stream-length 3)
 
-(define s-raingaugeB (new-behavior sym-boolean stream-length (* 2 stream-length)))
-(define s-clockB (new-behavior sym-integer stream-length (* 2 stream-length)))
-(define s-motion-sensorB (new-behavior sym-boolean stream-length (* 2 stream-length))) ;; or, should this be event stream?
+(define s-raingaugeB (new-behavior sym-boolean stream-length))
+(define s-clockB (new-behavior sym-time-vec stream-length))
+(define s-motionSensorB (new-behavior sym-boolean stream-length)) ;; or, should this be event stream?
+
+(define small-raingaugeB (behavior #f (list #t #f)))
+(define small-clockB (behavior (vector 17 5 9) (list (vector 18 0 1) (vector 18 2 0))))
+(define small-motionSensorB (behavior 'off (list 'off 'off)))
+(define small-sprinklersB (behavior 'off '(off off)))
+
+(define concrete-raingaugeB (behavior #f (list #t #t #f #f #f #f #f #f #f #f #f #f #f)))
+(define concrete-clockB (behavior (vector 15 3 0) (list (vector 17 0 0)
+                                                        (vector 18 0 0)
+                                                        (vector 18 0 5)
+                                                        (vector 0 0 0) ;; midnight
+                                                        (vector 8 0 0)
+                                                        (vector 13 0 5)
+                                                        (vector 18 0 0)
+                                                        (vector 18 0 3)
+                                                        (vector 18 0 6)
+                                                        (vector 18 0 7)
+                                                        (vector 18 0 8)
+                                                        (vector 18 1 1)
+                                                        (vector 18 1 3))))
+(define concrete-motionSensorB (behavior #f (list #f #f #t #f #t #f #f #f #t #t #f #f #f)))
+(define concrete-sprinklersB (behavior 'off '(off off off off off off on on off off on off off)))
+
+(define (saw-rain-last-dayB raingaugeB clockB)
+  (collectB #f (λ (r prev) (if (eq? r 'midnight) #f
+                               (if r #t prev)))
+            (liftB2 (λ (rain clock) (if (is-midnight? clock) 'midnight rain)) raingaugeB clockB)))
+
+(define (sprinklers-graph clockB motionSensorB raingaugeB)
+  (condB (list (list motionSensorB (constantB 'off))
+               (list (liftB2 (λ (rain clock) (and (not rain)
+                                                  (eq? (time-vec-hour clock) 18)
+                                                  (< (time-vec-min1 clock) 1)))
+                             (saw-rain-last-dayB raingaugeB clockB) clockB) (constantB 'on))
+               (list (constantB #t) (constantB 'off)))))
---
+++ racketimpl/straightline_synthesis/sprinklers.rkt
@@ -0,0 +1,56 @@
+#lang rosette
+
+(require "../dense-fjmodels.rkt")
+(require "../densefjapi.rkt")
+(require "../straightline.rkt")
+(require "../benchmarks/sprinklers.rkt")
+
+(define (straightline-sprinklers-graph clockB motionSensorB raingaugeB)
+  (define r1 clockB)
+  (define r2 motionSensorB)
+  (define r3 raingaugeB)
+  (define r4 (liftB2 (λ (rain clock) (if (is-midnight? clock) 'midnight rain)) r3 r1))
+  (define r5 (collectB #f (λ (r prev) (if (eq? r 'midnight) #f
+                               (if r #t prev))) r4))
+  (define r6 (constantB 'on))
+  (define r7 (liftB2 (λ (rain clock) (and (not rain)
+                                                  (eq? (time-vec-hour clock) 18)
+                                                  (< (time-vec-min1 clock) 1))) r5 r1))
+  (define r8 (constantB 'off))
+  (define r9 (constantB #t))
+  (define r10 (condB (list (list r2 r8)
+                           (list r7 r6)
+                           (list r9 r8))))
+  r1)
+
+#;(define v-binding (verify (assert (same sprinklers-graph
+                                        straightline-sprinklers-graph
+                                        s-clockB s-motionSensorB s-raingaugeB))))
+#;(if (unsat? v-binding)
+    (displayln "straightline program and implementation program are equal")
+    (displayln "no"))
+
+(define holes (for/list ([i (range 7)]) (get-insn-holes)))
+(define-symbolic* retval-idx integer?)
+
+(define (sketch-graph input1 input2 input3)
+  (define r1 input1)
+  (define r2 input2)
+  (define r3 input3)
+  (define r4 (single-insn (list-ref holes 0) (list r1 r2 r3)))
+  (define r5 (single-insn (list-ref holes 1) (list r1 r2 r3 r4)))
+  (define r6 (single-insn (list-ref holes 2) (list r1 r2 r3 r4 r5)))
+  (define r7 (single-insn (list-ref holes 3) (list r1 r2 r3 r4 r5 r6)))
+  (define r8 (single-insn (list-ref holes 4) (list r1 r2 r3 r4 r5 r6 r7)))
+  (define r9 (single-insn (list-ref holes 5) (list r1 r2 r3 r4 r5 r6 r7 r8)))
+  (define r10 (single-insn (list-ref holes 6) (list r1 r2 r3 r4 r5 r6 r7 r8 r9)))
+  (list-ref (list r1 r2 r3 r4 r5 r6 r7 r8 r9 r10) retval-idx))
+
+(define binding (time (synthesize #:forall (harvest s-clockB s-motionSensorB s-raingaugeB)
+                                  #:guarantee (same straightline-sprinklers-graph sketch-graph
+                                                    s-clockB s-motionSensorB s-raingaugeB))))
+
+(if (unsat? binding)
+    (displayln "unsat")
+    (print-from-holes holes retval-idx binding 7 3))
+
