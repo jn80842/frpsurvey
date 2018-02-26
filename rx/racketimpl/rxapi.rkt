e0e594403a3091efe21b5966893b3f7188742b37
Julie Newcomb <newcombj@cs.washington.edu>
2018-02-25 16:29:19 -0800

    more rxjs operators

--- rx/racketimpl/rxapi.rkt
+++ rx/racketimpl/rxapi.rkt
@@ -4,11 +4,13 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;;   Models
+;;   Helper methods
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
+(define (persistent-stream stream)
+  (for/list ([i (range (length stream))])
+    (foldl (λ (n m) (if (empty? n) m n)) '() (take stream (add1 i)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -16,7 +18,17 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; combineLatest
+;; waits for all observables to emit at least once
+;; thereafter, when any observable emits, send it with the most recent values of the others
+(define (combineLatestO . streams)
+  (let ([expanded-streams (map persistent-stream streams)])
+    (for/list ([i (range (length (first streams)))])
+      (let ([inputs (map (λ (e) (list-ref e i)) streams)]
+            [expanded (map (λ (e) (list-ref e i)) expanded-streams)])
+        (if (or (ormap empty? expanded)
+                (andmap empty? inputs))
+            '()
+            (flatten expanded))))))
 
 (define (fromEventO events)
   events)
@@ -43,7 +55,17 @@
 
 ;; dispose
 
-;; distinctUntilChanged
+(define (distinctUntilChangedO evt-stream)
+  (letrec ([f (λ (evt rest)
+                (cond [(empty? rest) evt]
+                      [(equal? evt (first rest)) '()]
+                      [(not (empty? (first rest))) evt]
+                      [else (f evt (cdr rest))]))])
+    (for/list ([i (range 1 (add1 (length evt-stream)))])
+      (let ([lst (take evt-stream i)])
+        (if (empty? (last lst))
+            '()
+            (f (last lst) (cdr (reverse lst))))))))
 
 (define (mapO proc . streams)
   (apply (curry map (λ es (let ([inputs (flatten es)])
@@ -64,18 +86,25 @@
               (if (empty? (list-ref rest-val i))
                   '()
                   (list (foldl (λ (n m) (if (empty? n) n
-                                            (apply proc (append (flatten n) (flatten m))))) init-val (take rest-val (add1 i)))))))))
+                                            (apply proc (append (flatten n) (flatten m)))))
+                               init-val (take rest-val (add1 i)))))))))
 (define (scanO-seed proc seed stream)
   (for/list ([i (range (length stream))])
     (if (empty? (list-ref stream i))
         '()
         (list (foldl (λ (n m) (if (empty? n) n
-                                  (apply proc (append (flatten n) (flatten m))))) seed (take stream (add1 i)))))))
+                                  (apply proc (append (flatten n) (flatten m)))))
+                     seed (take stream (add1 i)))))))
 
 ;; subscribe
 
-;; withLatestFrom
-
-
+;; after all observables have emitted at least one event
+;; when the source observable emits, send it with the most recent value from the other input observables
+;; can take optional function with which to combine the observable values; other send as arrays
+(define (withLatestFromO source-stream input-stream)
+  (let ([expanded-input (persistent-stream input-stream)])
+    (map (λ (e1 e2) (if (or (empty? e1) (empty? e2))
+                        '()
+                        (append e1 e2))) source-stream expanded-input)))
 
 
--- rx/racketimpl/test-rxapi.rkt
+++ rx/racketimpl/test-rxapi.rkt
@@ -9,6 +9,12 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(check-equal? (combineLatestO (list '() '(1) '() '(1))
+                              (list '() '() '(2) '(2))) (list '() '() '(1 2) '(1 2)))
+(check-equal? (combineLatestO (list '() '(1) '() '(2))
+                              (list '() '() '(3) '(4))
+                              (list '() '() '() '(10))) (list '() '() '() '(2 4 10)))
+
 (check-equal? (intervalO 3 10) (list '() '() '(1) '() '() '(2) '() '() '(3) '()))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -17,7 +23,15 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(check-equal? (distinctUntilChangedO (list '() '() '(1) '(1) '(2) '(1)))
+              (list '() '() '(1) '() '(2) '(1)))
+
 (check-equal? (mapO + (list '(1) '(2)) (list '(3) '(4))) (list '(4) '(6)))
 
 (check-equal? (scanO-no-seed + (list '(1) '(2) '(3))) (list '(1) '(3) '(6)))
 (check-equal? (scanO-seed + 10 (list '(1) '(2) '(3))) (list '(11) '(13) '(16)))
+
+(check-equal? (withLatestFromO (list '() '(3) '() '(9))
+                               (list '(1) '() '(2) '())) (list '() '(3 1) '() '(9 2)))
+(check-equal? (withLatestFromO (list '(3) '() '(1) '(0))
+                               (list '() '(1) '() '())) (list '() '() '(1 1) '(0 1)))
