da961141b2f8f950000e3b61c7c3e7e8e0e3a918
Julie Newcomb <newcombj@cs.washington.edu>
2018-05-29 18:51:45 -0700

    simpler lookup functions for multiply and divide

--- deepcoderlang/api.rkt
+++ deepcoderlang/api.rkt
@@ -83,88 +83,23 @@
       (for/list ([i (range (length xs))])
         (foldl f (first xs) (take (drop xs 1) i)))))
 
-(define-symbolic expt-uf (~> integer? integer?))
-
-(define (get-lookup-expt magnitude)
-  (displayln (format "Synthesizing lookup expt 2 with range -~a-~a" magnitude magnitude))
-  (clear-asserts!)
-  (define-symbolic i integer?)
-  (assert (<= i magnitude))
-  (assert (>= i 0))
-  (define b (time (synthesize #:forall (list i)
-                              #:guarantee (assert (eq? (expt i 2) (expt-uf i))))))
-  (clear-asserts!)
-  (let ([e2 (evaluate expt-uf b)])
-    (λ (x)
-      (assert (<= (abs x) magnitude))
-      (assert (>= (abs x) 0))
-      (e2 (abs x)))))
+(define (get-lookup-expt magnitude)
+  (λ (i) (list-ref (map (λ (x) (expt x 2)) (range (add1 magnitude))) (abs i))))
 
 (define lut-expt (get-lookup-expt 10))
 
-(define-symbolic div2-uf (~> integer? integer?))
-
-(define (signed-div-uf uf)
-  (λ (x)
-    (if (negative? x)
-        (- (uf x))
-        (uf x))))
-
 (define (get-lookup-div2 magnitude)
-  (displayln (format "Synthesizing lookup div2 with range -~a-~a" magnitude magnitude))
-  (clear-asserts!)
-  (define-symbolic i integer?)
-  (assert (<= (abs i) magnitude))
-  (assert (>= (abs i) 0))
-  (define b (let ([signed-div2 (signed-div-uf div2-uf)])
-              (time (synthesize #:forall (list i)
-                                #:guarantee (assert (eq? (quotient i 2) (signed-div2 i)))))))
-  (clear-asserts!)
-  (let ([q2 (signed-div-uf (evaluate div2-uf b))])
-    (λ (x)
-      (assert (<= (abs x) magnitude))
-      (assert (>= (abs x) 0))
-      (q2 x))))
+  (λ (x) (list-ref (map (λ (i) (quotient i 2)) (range (- magnitude) (add1 magnitude))) (+ x magnitude))))
 
-(define lut-div2 (get-lookup-div2 10))
-
-(define-symbolic div3-uf (~> integer? integer?))
+(define lut-div2 (λ (i) (quotient i 2)));(get-lookup-div2 10))
 
 (define (get-lookup-div3 magnitude)
-  (displayln (format "Synthesizing lookup div3 with range -~a-~a" magnitude magnitude))
-  (clear-asserts!)
-  (define-symbolic i integer?)
-  (assert (<= (abs i) magnitude))
-  (assert (>= (abs i) 0))
-  (define b (let ([signed-div3 (signed-div-uf div3-uf)])
-              (time (synthesize #:forall (list i)
-                                #:guarantee (assert (eq? (quotient i 3) (signed-div3 i)))))))
-  (clear-asserts!)
-  (let ([q3 (signed-div-uf (evaluate div3-uf b))])
-    (λ (x)
-      (assert (<= (abs x) magnitude))
-      (assert (>= (abs x) 0))
-      (q3 x))))
+  (λ (x) (list-ref (map (λ (i) (quotient i 3)) (range (- magnitude) (add1 magnitude))) (+ x magnitude))))
 
 (define lut-div3 (get-lookup-div3 10))
 
-(define-symbolic div4-uf (~> integer? integer?))
-
 (define (get-lookup-div4 magnitude)
-  (displayln (format "Synthesizing lookup div4 with range -~a-~a" magnitude magnitude))
-  (clear-asserts!)
-  (define-symbolic i integer?)
-  (assert (<= (abs i) magnitude))
-  (assert (>= (abs i) 0))
-  (define b (let ([signed-div4 (signed-div-uf div4-uf)])
-              (time (synthesize #:forall (list i)
-                                #:guarantee (assert (eq? (quotient i 4) (signed-div4 i)))))))
-  (clear-asserts!)
-  (let ([q4 (signed-div-uf (evaluate div4-uf b))])
-    (λ (x)
-      (assert (<= (abs x) magnitude))
-      (assert (>= (abs x) 0))
-      (q4 x))))
+  (λ (x) (list-ref (map (λ (i) (quotient i 4)) (range (- magnitude) (add1 magnitude))) (+ x magnitude))))
 
 (define lut-div4 (get-lookup-div4 10))
 
@@ -189,21 +124,8 @@
                                       "(λ (i) (/ i 4))"
                                       ))
 
-(define-symbolic odd-lut? (~> integer? boolean?))
-
 (define (get-lookup-odd magnitude)
-  (displayln (format "Synthesizing lookup odd? with range -~a-~a" magnitude magnitude))
-  (clear-asserts!)
-  (define-symbolic i integer?)
-  (assert (<= (abs i) magnitude))
-  (define b (time (synthesize #:forall (list i)
-                              #:guarantee (assert (eq? (odd? i) (odd-lut? (abs i)))))))
-  (clear-asserts!)
-  (let ([o? (evaluate odd-lut? b)])
-    (λ (x)
-      (assert (<= (abs x) magnitude))
-      (assert (>= (abs x) 0))
-      (o? (abs x)))))
+  (λ (x) (list-ref (map (λ (i) (odd? i)) (range (- magnitude) (add1 magnitude))) (+ x magnitude))))
 
 (define lut-odd10? (get-lookup-odd 10))
 
@@ -218,7 +140,11 @@
                                        "negative?"
                                        ))
 
-(define-symbolic times (~> integer? integer? integer?))
+(define (get-lookup-pos-mult magnitude)
+  (λ (x y) (list-ref
+            (list-ref (for/list ([i (range (add1 magnitude))])
+                        (for/list ([j (range (add1 magnitude))])
+                          (* i j))) x) y)))
 
 (define (signed-times uf)
   (λ (x y)
@@ -230,42 +156,8 @@
             (- (uf (abs x) (abs y)))
             (uf x y))))))
 
-(define (multiply-function times-f)
-  (λ (x y)
-    (let ([signed-uf (signed-times times-f)])
-      (cond [(and (concrete-integer? x) (concrete-integer? y)) (* x y)]
-            [(= x 0) 0]
-            [(= y 0) 0]
-            [(= x 1) y]
-            [(= y 1) x]
-            [(= x -1) (- y)]
-            [(= y -1) (- x)]
-            [(= x 2) (+ y y)]
-            [(= y 2) (+ x x)]
-            [(= x 3) (+ y y y)]
-            [(= y 3) (+ x x x)]
-            [(= x -2) (- (+ y y))]
-            [(= y -2) (- (+ x x))]
-            [(= x -3) (- (+ y y y))]
-            [(= y -3) (- (+ x x x))]
-            [(<= x y) (signed-uf x y)]
-            [else (signed-uf y x)]))))
-
 (define (get-lookup-mult magnitude)
-  (displayln (format "Synthesizing lookup multiplication with range ~a-~a" (- magnitude) magnitude ))
-  (define-symbolic i1 integer?)
-  (define-symbolic i2 integer?)
-  (clear-asserts!)
-  (assert (<= (abs i1) magnitude))
-  (assert (<= (abs i2) magnitude))
-  (define b (time (synthesize #:forall (list i1 i2)
-                              #:guarantee (assert (= ((multiply-function times) i1 i2) (* i1 i2))))))
-  (clear-asserts!)
-  (let ([m (multiply-function (evaluate times b))])
-    (λ (x y)
-      (begin (assert (<= (abs x) magnitude))
-             (assert (<= (abs y) magnitude))
-             (m x y)))))
+  (signed-times (get-lookup-pos-mult magnitude)))
 
 (define lut-mult10 (get-lookup-mult 10))
 
