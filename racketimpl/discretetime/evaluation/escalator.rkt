ce0119772dc05450f95472796c0950f010a1c714
Julie Newcomb <newcombj@cs.washington.edu>
2018-11-05 23:55:34 -0800

    full spec from Finkbeiner et al escalator problem

--- racketimpl/discretetime/evaluation/escalator.rkt
+++ racketimpl/discretetime/evaluation/escalator.rkt
@@ -5,15 +5,7 @@
 (require "../operators.rkt")
 (require "../sketch.rkt")
 
-(current-bitwidth 5)
-
-;; streams
-;; topSensorE
-;; bottomSensorE
-;; stepMovementE
-;; stepsMovementB
-;; userCounterB
-;; userCounterE
+(current-bitwidth #f)
 
 (define ENTER 0)
 (define EXIT 1)
@@ -23,16 +15,20 @@
 (define STOP 2)
 
 (define timebound 4)
+(define sTop (new-event-stream get-sym-int timebound))
+(define sBottom (new-event-stream get-sym-int timebound))
+(define sSteps (new-behavior get-sym-int timebound))
+(define sUsers (new-behavior get-sym-int timebound))
 
 (define (iff p q)
   (or (and p q) (and (not p) (not q))))
 
-;; LTL always/globally operator over event stream
-(define (always predicate events)
-  (andmap predicate events))
+(define (remove-last l)
+  (reverse (cdr (reverse l))))
 
-(define (nextE predicate events)
-  #t)
+;; LTL always/globally operator over event stream
+(define (always predicate . events)
+  (apply (curry andmap predicate) events))
 
 ;; redefine psi a bit
 ;; original psi states that various events can't occur at the same time
@@ -50,63 +46,85 @@
 ;; ((enterEvent(bottom) && !exitEvent(top) && !(steps == MOVEDOWN))
 ;; || (enterEvent(top) && !exitEvent(bottom) && !(steps == MOVEUP)))
 ;; <-> users := users + 1
-(define (theta0 topSensorE bottomSensorE stepsMovementB userCounterE)
-  (let ([stepsStatusE (behavior-changes stepsMovementB)])
+;; simplify users to a stream of +1 and -1 to user count
+(define (theta0 topSensorE bottomSensorE stepsMovementB userCounterB)
+  (let* ([stepsStatusE (behavior-changes stepsMovementB)]
+        [calculateDiff (λ (b1 b2) (- (- b1 b2)))]
+        [userCountDiffs (append (list (calculateDiff (behavior-init userCounterB) (first (behavior-changes userCounterB)))) (map calculateDiff (remove-last (behavior-changes userCounterB)) (cdr (behavior-changes userCounterB))))])
     (andmap (λ (top bottom steps userCounter)
               (iff (or (and (equal? bottom ENTER) (not (equal? top EXIT)) (not (equal? steps MOVEDOWN)))
                        (and (equal? top ENTER) (not (equal? bottom EXIT)) (not (equal? steps MOVEUP))))
-                   (equal? userCounter 1))) topSensorE bottomSensorE stepsStatusE userCounterE)))
-(define (t0a t b s u)
-  (let ([stepsStatusE (behavior-changes s)])
-    (andmap (λ (top bottom steps userCounter)
-              (iff (and (equal? bottom ENTER) (not (equal? top EXIT)) (not (equal? steps MOVEDOWN)))
-                   (equal? userCounter 1))) t b stepsStatusE u)))
-
-;; reference implementation that matches theta0
-(define (refTheta0 top bottom steps)
-  (mergeE (constantE 1 bottom) (constantE 1 top)))
-
-;; synthesize userCounterE from topSensorE, bottomSensorE, stepsMovementB inputs
-(define sTopE (new-event-stream get-sym-int timebound))
-(define sBottomE (new-event-stream get-sym-int timebound))
-(define sStepsB (new-behavior get-sym-int timebound))
-
-(define (psi topE bottomE stepsB)
-  (begin (assert (sensor topE))
-         (assert (sensor bottomE))
-         (assert (steps stepsB))))
-
-(define sk (get-symbolic-sketch 5 (make-vector 5 #f) 3))
-
-(define (t0syn)
-  (begin
-    (psi sTopE sBottomE sStepsB)
-    (let* ([evaled-sk ((get-sketch-function sk) sTopE sBottomE sStepsB)]
-           [b (time (synthesize #:forall (symbolics (list sTopE sBottomE sStepsB))
-                                #:guarantee (assert (t0a sTopE sBottomE sStepsB evaled-sk))))])
-      (if (unsat? b)
-          (print "unsat")
-          (print-sketch sk b)))))
+                   (equal? userCounter 1))) topSensorE bottomSensorE stepsStatusE userCountDiffs)))
 
 ;; theta one
-(define (theta1 topSensorE bottomSensorE stepsMovementB userCounterE)
-  (let ([stepsStatusE (behavior-changes stepsMovementB)])
+;; ((exitEvent(top) && !enterEvent(bottom) && !(steps == MOVEDOWN))
+;; || (exitEvent(bottom) && !enterEvent(top) && !(steps == MOVEUP)))
+;; <-> users := users - 1
+(define (theta1 topSensorE bottomSensorE stepsMovementB userCounterB)
+  (let* ([stepsStatusE (behavior-changes stepsMovementB)]
+         [calculateDiff (λ (b1 b2) (- (- b1 b2)))]
+         [userCountDiffs (append (list (calculateDiff (behavior-init userCounterB) (first (behavior-changes userCounterB)))) (map calculateDiff (remove-last (behavior-changes userCounterB)) (cdr (behavior-changes userCounterB))))])
     (andmap (λ (top bottom steps userCounter)
               (iff (or (and (equal? top EXIT) (not (equal? bottom ENTER)) (not (equal? steps MOVEDOWN)))
                        (and (equal? bottom EXIT) (not (equal? top ENTER)) (not (equal? steps MOVEUP))))
-                   (equal? userCounter -1))) topSensorE bottomSensorE stepsStatusE userCounterE)))
+                   (equal? userCounter -1))) topSensorE bottomSensorE stepsStatusE userCountDiffs)))
 ;; theta two
-(define (theta2 stepsMovementE userCounterB bottomSensorE)
-  (let ([userCounter (behavior-changes userCounterB)])
+;; (stepMovementChangeE == MOVEUP) --> userCounterB == 0 && enterEvent(bottom)
+(define (theta2 topSensorE bottomSensorE stepsMovementB userCounterB)
+  (let ([userCounterE (behavior-changes userCounterB)]
+        [stepsMovementE (behavior-changes stepsMovementB)])
     (andmap (λ (steps users bottom)
               (implies (equal? steps MOVEUP) (and (equal? users 0) (equal? bottom ENTER))))
-            stepsMovementE userCounter bottomSensorE)))
+            stepsMovementE userCounterE bottomSensorE)))
 
 ;; theta three
-(define (theta3 stepsMovementE userCounterB topSensorE)
-  (let ([userCounter (behavior-changes userCounterB)])
-    (andmap (λ (steps users top)
+;; (stepMovementChangeE == MOVEDOWN) --> userCounterB == 0 && enterEvent(top)
+(define (theta3 topSensorE bottomSensorE stepsMovementB userCounterB)
+  (let ([userCounterE (behavior-changes userCounterB)]
+        [stepsMovementE (behavior-changes stepsMovementB)])
+    (always (λ (steps users top)
               (implies (equal? steps MOVEDOWN) (and (equal? users 0) (equal? top ENTER))))
-            stepsMovementE userCounter topSensorE)))
+            stepsMovementE userCounterE topSensorE)))
 
 ;; theta four
+;; userCounterB == 0 && enterEvent(bottom) && !enterEvent(top) --> stepsMovementChangeE == MOVEUP
+(define (theta4 topSensorE bottomSensorE stepsMovementB userCounterB)
+  (let ([stepsMovementChangeE (changes stepsMovementB)]
+        [userCounterE (behavior-changes userCounterB)])
+    (always (λ (top bottom steps users)
+              (implies (and (equal? users 0) (equal? bottom ENTER) (not (equal? top ENTER)))
+                       (equal? steps MOVEUP)))
+            topSensorE bottomSensorE stepsMovementChangeE userCounterE)))
+
+;; theta five
+;; userCounterB == 0 && enterEvent(top) && !enterEvent(bottom) --> stepsMovementChangeE == MOVEDOWN
+(define (theta5 topSensorE bottomSensorE stepsMovementB userCounterB)
+  (let ([stepsMovementChangeE (changes stepsMovementB)]
+        [userCounterE (behavior-changes userCounterB)])
+    (always (λ (top bottom steps users)
+              (implies (and (equal? users 0) (equal? top ENTER) (not (equal? bottom ENTER)))
+                       (equal? steps MOVEDOWN)))
+            topSensorE bottomSensorE stepsMovementChangeE userCounterE)))
+
+;; theta six
+;; userCounterB == 0 && enterEvent(top) && enterEvent(bottom) --> stepsMovementChangeE == MOVEDOWN || stepsMovementChangeE == MOVEUP
+(define (theta6 topSensorE bottomSensorE stepsMovementB userCounterB)
+  (let ([stepsMovementChangeE (changes stepsMovementB)]
+        [userCounterE (behavior-changes userCounterB)])
+    (always (λ (top bottom steps users)
+              (implies (and (equal? users 0) (equal? top ENTER) (equal? bottom ENTER))
+                       (or (equal? steps MOVEUP) (equal? steps MOVEDOWN))))
+            topSensorE bottomSensorE stepsMovementChangeE userCounterE)))
+
+;; theta seven
+;; ! users == 0 && X (users == 0 && ! enterEvent(top) && ! enterEvent(bottom)) <-> steps := STOP
+(define (theta7 topSensorE bottomSensorE stepsMovementB userCounterB)
+  (let* ([userCounterE (behavior-changes userCounterB)]
+         [nextUserCounterE (cdr userCounterE)]
+         [nextTopSensorE (cdr topSensorE)]
+         [nextBottomSensorE (cdr bottomSensorE)]
+         [stepsMovementChangeE (changes stepsMovementB)])
+    (always (λ (users xUsers xTop xBottom steps)
+              (iff (and (not (equal? users 0)) (equal? xUsers 0) (not (equal? xTop ENTER)) (not (equal? xBottom ENTER)))
+                   (equal? steps STOP)))
+            (remove-last userCounterE) nextUserCounterE nextTopSensorE nextBottomSensorE (remove-last stepsMovementChangeE))))
